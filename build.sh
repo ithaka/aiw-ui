ba82e63c525fb8f28682c7875a56e2a24be995e2
Cody <X@X.local>
2016-11-07 12:11:19 -0500

    add webpack-merge to build

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ gem install sass
 export PATH=/usr/local/node-v6.3.1-linux-x64/bin:$PATH
 
 # Node bits
-npm install --global webpack webpack-dev-server karma karma-cli protractor typescript rimraf
+npm install --global webpack webpack-dev-server webpack-merge karma karma-cli protractor typescript rimraf
 
 # bower --allow-root install
 
