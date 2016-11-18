2304e4931f3e405b6f4c571f51df7463ad709bc1
Cody <X@X.local>
2016-11-18 07:49:54 -0600

    Add footer

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer',
+  providers: [
+    
+  ],
+  templateUrl: './footer.component.html',
+  styleUrls: [ './footer.component.scss' ],
+})
+export class Footer {
+  
+  // TypeScript public modifiers
+  constructor( ) { 
+    
+  }
+
+  
+  ngOnInit() {
+    
+  }
+}
