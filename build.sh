9f336a57da3b60b31933765c5a1353781cd79807
Cody <X@X.local>
2016-12-08 13:24:44 -0600

    Add global phantomjs dependency

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ export PATH=/usr/local/node-v6.9.1-linux-x64/bin:$PATH
 npm install --global yarn
 
 # Install global build packages
-yarn global add webpack webpack-dev-server karma karma-cli protractor typescript rimraf npm npm-install-missing
+yarn global add webpack webpack-dev-server karma karma-cli protractor typescript rimraf phantomjs
 
 # Install dependencies
 yarn install
