ae45d2655edd88f7ae396d5e452fdc438aeaf22c
Cody <pr@cdy.pm>
2016-12-09 11:11:07 -0600

    Initial auth changes

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Locker } from 'angular2-locker';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import {
   CanActivate,
   Router,
@@ -13,20 +14,62 @@ export class AuthService implements CanActivate {
   
   // Use header rewrite proxy for local development
   private proxyUrl = 'http://rocky-cliffs-9470.herokuapp.com/api?url=';
-  // private instance var for base url
   private baseUrl = this.proxyUrl + 'http://library.artstor.org/library/secure';
-  
 
-  constructor(private _router:Router, locker:Locker) {
+  constructor(private _router:Router, locker:Locker, private http: Http) {
     this._storage = locker;
     this._router = _router;
   }
-  
 
-  //check user authentication
+  public formEncode(obj: Object): string {
+      var encodedString = '';
+      for (var key in obj) {
+          if (encodedString.length !== 0) {
+              encodedString += '&';
+          }
+          encodedString += key + '=' + encodeURIComponent(obj[key]);
+      }
+      return encodedString.replace(/%20/g, '+');
+  }
+
+  public extractData(res: Response): Object {
+      let body = res.json();
+      return body || { };
+  }
+
+  public getProxyUrl(): string {
+    return this.proxyUrl;
+  }
+  
+  public getUrl(): string {
+    return this.baseUrl;
+  }
+
+  public saveUser(user: any) {
+        this._storage.set('user', user); 
+  }
+
+  public getUser() : any {
+      return this._storage.get('user');
+  }
+
+  // Check user authentication
   isAuthenticated(): boolean {
-    if (this._storage.get('user') === null || this._storage.get('user') === {}) {
-      return false;
+    if (this.getUser() === null || this.getUser() === {}) {
+      this.getUserInfo()
+        .then(
+          data  => function(data) {
+            console.log(data);
+          },
+          error =>  function(error) {
+
+          }
+        ).catch(function(err) {
+          // 
+        });
+
+    } else {
+      return true;
     }
     return true;
   }
@@ -41,5 +84,11 @@ export class AuthService implements CanActivate {
     return authenticated;
   }
 
-  //TODO build in observable for authentication state
+  getUserInfo() {
+    let options = new RequestOptions({ withCredentials: true });
+    return this.http
+      .get(this.getUrl() + '/userinfo', options)
+      .toPromise()
+      .then(this.extractData);
+  }
 }
