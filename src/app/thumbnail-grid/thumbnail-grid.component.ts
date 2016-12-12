980b30a1a3183cf077da9ed2abac59b3bd701412
Corbfon <corbin.fonville@gmail.com>
2016-12-12 11:05:43 -0600

    init thumbnail-grid component

---
+++ src/app/thumbnail-grid/thumbnail-grid.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { AssetService } from '../home/assets.service';
+
+
+@Component({
+  selector: 'ang-thumbnail-image',
+  providers: [AssetService],
+  styleUrls: [],
+  templateUrl: './image-group.component.html'
+})
+export class imageGroup {
+  
+}
