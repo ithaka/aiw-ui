25fae46d1a1694156147f237467babba23a0be35
Corbfon <corbin.fonville@gmail.com>
2016-12-23 16:35:56 -0600

    Revert "Revert "revert 105055""
    This reverts commit 7c4e00021930510ff4743fe7993444dd2f47760e.

--- src/app/browse-page/commons.component.ts
+++ src/app/browse-page/commons.component.ts
@@ -26,14 +26,7 @@ export class BrowseCommonsComponent implements OnInit {
           console.log(data);
           data.Collections.forEach((collection, index) => {
             // let isFolder = collection.isFolder;
-            this.tags.push(new Tag(
-                                    collection.collectionid,
-                                    collection.collectionname,
-                                    true,
-                                    null, 
-                                    { label: "collection", folder: true }, 
-                                    this.toggleFolder,
-                                    { _assets: this._assets }));
+            this.tags.push(new Tag(collection.collectionid, collection.collectionname, true, null, { label: "collection", folder: true }));
           });
         }
 
@@ -42,51 +35,5 @@ export class BrowseCommonsComponent implements OnInit {
       .catch((err) => {
         console.error(err);
       });
-  }
-
-    private toggleFolder(tag: Tag, helper: any): Promise<Tag[]> {
-      console.log("passed funciton got called!");
-      console.log(tag);
-
-      console.log(this.toggleFolder);
-
-    // has the tag been opened before?
-    if (!tag.touched) {
-      //the tag doesn't have any children, so we run a call to get any
-      let childArr: Tag[] = [];
-      console.log("we're about to search stuff!");
-      
-      // logic determines which call to make, to categories or subcategories
-      if (tag.type.label === "collection") {
-        return helper._assets.category(tag.tagId)
-          .then((data) => {
-            console.log(data);
-            for(let category of data.Categories) {
-              let categoryTag = new Tag(category.widgetId, category.title, true, tag, { label: "category", folder: category.isFolder }, this.toggleFolder, helper);
-              console.log(categoryTag);
-              childArr.push(categoryTag);
-            }
-            return childArr;
-          });
-          // .catch((err) => {
-          //   console.error(err);
-          // });
-      } else if (tag.type.label === "category") {
-        return helper._assets.subcategories(tag.tagId)
-          .then((data) => {
-            for(let category of data) {
-              let categoryTag = new Tag(category.widgetId, category.title, true, tag, { label: "subcategory", folder: category.isFolder }, this.toggleFolder, helper);
-              childArr.push(categoryTag);
-            }
-            return childArr;
-          });
-          // .catch((err) => {
-          //   console.error(err);
-          // });
-      }
-
-    } else {
-      return;
-    }
   }
 }
