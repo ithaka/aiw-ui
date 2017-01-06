d345a891510dc6ce6815bd7a7e648b2243e66f10
Corbfon <corbin.fonville@gmail.com>
2017-01-06 14:57:22 -0600

    store one version of every route.routerConfig.path one time upon navigation

--- src/app/reuse-strategy.ts
+++ src/app/reuse-strategy.ts
@@ -2,10 +2,21 @@
 
 import { ActivatedRouteSnapshot, RouteReuseStrategy, DetachedRouteHandle } from '@angular/router';
 
+interface RouteStorageObject {
+    snapshot: ActivatedRouteSnapshot;
+    handle: DetachedRouteHandle;
+}
+
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
-    handlers: {[key: string]: DetachedRouteHandle} = {};
-    // handlers: {[key: string]: any} = {};
+    // storedRoutes: {
+    //     [key: string]: {
+    //         snapshot: ActivatedRouteSnapshot,
+    //         handle: DetachedRouteHandle
+    //     }
+    // } = {};
+    storedRoutes: { [key: string]: RouteStorageObject } = {};
+    // storedRoutes: {[key: string]: any} = {};
 
     /** 
      * Decides when the route should be stored
@@ -22,35 +33,66 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
      * Should also check to make sure we're not storing too many handles, or handles for the same component
      */
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+        let storedRoute: RouteStorageObject = {
+            snapshot: route,
+            handle: handle
+        };
+
         // console.debug('CustomReuseStrategy:store', route, handle);
-        console.log("storing into: ", this.handlers, "store:", handle);
+        console.log("storing into: ", this.storedRoutes, "store:", storedRoute);
         // routes are stored by path - the key is the path name, and the handle is stored under it so that you can only ever have one object stored for a single path
-        this.handlers[route.routeConfig.path] = handle;
-        // console.log(this.handlers);
+        this.storedRoutes[route.routeConfig.path] = storedRoute;
+        // console.log(this.storedRoutes);
     }
 
     
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
         // console.debug('CustomReuseStrategy:shouldAttach', route);
-        console.log("attaching: ", route, "return: ", !!route.routeConfig && !!this.handlers[route.routeConfig.path]);
+        
 
-        // this will be true if the route has been navigated to before
-        let canAttach: boolean = !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+        // this will be true if the route has been stored before
+        let canAttach: boolean = !!route.routeConfig && !!this.storedRoutes[route.routeConfig.path];
+
+        // at this point we already know that the paths match because the storedResults key is the route.routeConfig.path
+        // so, if the query params also match, then we should reuse the component
+        if (canAttach) {
+            let willAttach: boolean = true;
+            console.log("param comparison:");
+            console.log(this.compareObjects(route.params, this.storedRoutes[route.routeConfig.path].snapshot.params));
+            console.log("query param comparison");
+            console.log(this.compareObjects(route.queryParams, this.storedRoutes[route.routeConfig.path].snapshot.queryParams));
+
+
+            let paramsMatch: boolean = this.compareObjects(route.params, this.storedRoutes[route.routeConfig.path].snapshot.params);
+            let queryParamsMatch: boolean = this.compareObjects(route.queryParams, this.storedRoutes[route.routeConfig.path].snapshot.queryParams);
+
+
+            // console.log("attaching: ", route, "match:", this.storedRoutes[route.routeConfig.path].snapshot, "return: ", this.storedRoutes[route.routeConfig.path].snapshot === route);
+            console.log("attaching...", route, "does it match?", this.storedRoutes[route.routeConfig.path].snapshot, "return: ", paramsMatch && queryParamsMatch);
+            // let queryParamsEqual = this.storedRoutes[route.routeConfig.path].snapshot.queryParams === route.queryParams;
+            // return true only if the ActivatedRouteSnapshot from the stored object and the future object are equal
+            // return this.storedRoutes[route.routeConfig.path].snapshot === route;
+            return paramsMatch && queryParamsMatch;
+        } else {
+            return false;
+        }
+
         // this decides whether the route already stored should be rendered in place of the requested route, and is the return value
-        // should check whether the route.routeConfig.path === this.handlers[route.routeConfig.path]
-        // let letsAttach: boolean = this.handlers[route.routeConfig.path] === 
-        // console.log(this.handlers[route.routeConfig.path].route.value);
-        return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+        // should check whether the route.routeConfig.path === this.storedRoutes[route.routeConfig.path]
+        // let letsAttach: boolean = this.storedRoutes[route.routeConfig.path] === 
+        // console.log(this.storedRoutes[route.routeConfig.path].route.value);
+        // return !!route.routeConfig && !!this.storedRoutes[route.routeConfig.path];
         // return false;
     }
 
     /** Returns stored instance of component at route, if it exists */
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
         // console.debug('CustomReuseStrategy:retrieve', route);
-        if (!route.routeConfig) return null;
-        console.log("retrieving", "return: ", this.handlers[route.routeConfig.path]);
+        // return null if the path does not have a routerConfig OR if there is no stored route for that routerConfig
+        if (!route.routeConfig || !this.storedRoutes[route.routeConfig.path]) return null;
+        console.log("retrieving", "return: ", this.storedRoutes[route.routeConfig.path]);
         /** returns handle when the route.routeConfig.path is already stored */
-        return this.handlers[route.routeConfig.path];
+        return this.storedRoutes[route.routeConfig.path].handle;
     }
 
     /** When return value is false, the controller triggers this.retrieve */
@@ -61,4 +103,32 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
         return future.routeConfig === curr.routeConfig;
     }
 
+    private compareObjects(base: any, compare: any): boolean {
+
+        // loop through all properties in base object
+        for (let baseProperty in base) {
+
+            // determine if comparrison object has that property, if not: return false
+            if (compare.hasOwnProperty(baseProperty)) {
+                switch(typeof base[baseProperty]) {
+                    // if one is object and other is not: return false
+                    // if they are both objects, recursively call this comparison function
+                    case 'object':
+                        if ( typeof compare[baseProperty] !== 'object' || !this.compareObjects(base[baseProperty], compare[baseProperty]) ) { return false; } break;
+                    // if one is function and other is not: return false
+                    // if both are functions, compare function.toString() results
+                    case 'function':
+                        if ( typeof compare[baseProperty] !== 'function' || base[baseProperty].toString() !== compare[baseProperty].toString() ) { return false; } break;
+                    // otherwise, see if they are equal using coercive comparison
+                    default:
+                        if ( base[baseProperty] != compare[baseProperty] ) { return false; }
+                }
+            } else {
+                return false;
+            }
+        }
+
+        // returns true only after false HAS NOT BEEN returned through all loops
+        return true;
+    }
 }
