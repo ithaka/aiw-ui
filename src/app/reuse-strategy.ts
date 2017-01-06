3db163e35646135ebdea49e078e8dbd24502334e
Corbfon <corbin.fonville@gmail.com>
2017-01-06 12:58:48 -0600

    loads of debugging comments

--- src/app/reuse-strategy.ts
+++ src/app/reuse-strategy.ts
@@ -5,31 +5,59 @@ import { ActivatedRouteSnapshot, RouteReuseStrategy, DetachedRouteHandle } from 
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
     handlers: {[key: string]: DetachedRouteHandle} = {};
+    // handlers: {[key: string]: any} = {};
 
+    /** 
+     * Decides when the route should be stored
+     * If the route should be stored, I believe the boolean is indicating to a controller whether or not to fire this.store
+     */
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        console.debug('CustomReuseStrategy:shouldDetach', route);
+        // console.debug('CustomReuseStrategy:shouldDetach', route);
+        console.log("detaching", route, "return: ", true);
         return true;
     }
 
+    /**
+     * Handles route storage
+     * Should also check to make sure we're not storing too many handles, or handles for the same component
+     */
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
         // console.debug('CustomReuseStrategy:store', route, handle);
+        console.log("storing into: ", this.handlers, "store:", handle);
+        // routes are stored by path - the key is the path name, and the handle is stored under it so that you can only ever have one object stored for a single path
         this.handlers[route.routeConfig.path] = handle;
+        // console.log(this.handlers);
     }
 
+    
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
         // console.debug('CustomReuseStrategy:shouldAttach', route);
+        console.log("attaching: ", route, "return: ", !!route.routeConfig && !!this.handlers[route.routeConfig.path]);
+
+        // this will be true if the route has been navigated to before
+        let canAttach: boolean = !!route.routeConfig && !!this.handlers[route.routeConfig.path];
+        // this decides whether the route already stored should be rendered in place of the requested route, and is the return value
+        // should check whether the route.routeConfig.path === this.handlers[route.routeConfig.path]
+        // let letsAttach: boolean = this.handlers[route.routeConfig.path] === 
+        // console.log(this.handlers[route.routeConfig.path].route.value);
         return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
         // return false;
     }
 
+    /** Returns stored instance of component at route, if it exists */
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
         // console.debug('CustomReuseStrategy:retrieve', route);
         if (!route.routeConfig) return null;
+        console.log("retrieving", "return: ", this.handlers[route.routeConfig.path]);
+        /** returns handle when the route.routeConfig.path is already stored */
         return this.handlers[route.routeConfig.path];
     }
 
+    /** When return value is false, the controller triggers this.retrieve */
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
         // console.debug('CustomReuseStrategy:shouldReuseRoute', future, curr);
+        console.log("deciding");
+        // console.log('deciding', future.routeConfig, curr.routeConfig, "return: ", future.routeConfig === curr.routeConfig);
         return future.routeConfig === curr.routeConfig;
     }
 
