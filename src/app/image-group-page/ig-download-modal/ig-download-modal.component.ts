9e609524955078cce2d1ef22c4a82562b138d875
Corbfon <corbin.fonville@gmail.com>
2017-01-09 15:32:09 -0600

    loading symbol

--- src/app/image-group-page/ig-download-modal/ig-download-modal.component.ts
+++ src/app/image-group-page/ig-download-modal/ig-download-modal.component.ts
@@ -20,16 +20,19 @@ export class PptModalComponent implements OnInit {
   @Input()
   private ig: ImageGroup;
 
+  private isLoading: boolean = false;
+
   constructor(private _assets: AssetService) { }
 
   ngOnInit() { }
 
   private downloadImageGroup() {
+    this.isLoading = true;
     // make call to get number of allowed downloads
     // not sure which service to call yet - contacted Will about it
     this._assets.downloadPpt(this.ig).take(1).subscribe(
-      (data) => { console.log(data); },
-      (error) => { console.log(error); }
+      (data) => { console.log(data); this.isLoading = false; },
+      (error) => { console.log(error); this.isLoading = false; }
     )
   }
 }
