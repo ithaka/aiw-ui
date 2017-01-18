ae10ee6b0bf60d66129b9e8d9cdc690c4805a8de
Corbfon <corbin.fonville@gmail.com>
2017-01-18 11:14:36 -0600

    add more user types

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -69,6 +69,7 @@ export class Login {
       data.user.hasOwnProperty("isLoggedIn") && this.angulartics.setUserProperties.next({ isLoggedIn: data.user.isLoggedIn });
       data.user.hasOwnProperty("shibbolethUser") && this.angulartics.setUserProperties.next({ shibbolethUser: data.user.shibbolethUser });
       data.user.hasOwnProperty("dept") && this.angulartics.setUserProperties.next({ dept: data.user.dept });
+      data.user.hasOwnProperty("ssEnabled") && this.angulartics.setUserProperties.next({ ssEnabled: data.user.ssEnabled })
 
       data.user.isLoggedIn = true;
       this._auth.saveUser(data.user);
