91e20eb613ed68aa5e88fe6f1b887595640d57cc
Corbfon <corbin.fonville@gmail.com>
2017-01-26 11:04:32 -0600

    column styling for mobile nav

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 })
 export class NavMenu {
 
-  private mobileCollapsed: boolean = false;
+  private mobileCollapsed: boolean = true;
   
   // TypeScript public modifiers
   constructor( ) { 
