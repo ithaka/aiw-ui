b2e1afc400e045c7136cb197ab849c6c6e6573d3
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2017-01-31 17:17:23 +0500

    Previously applied filters should be cleared whenever a new term is searched.

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router'
 import { Subscription }   from 'rxjs/Subscription';
 
 import { AssetService } from './../shared/assets.service';
+import { AssetFiltersService } from '../asset-filters/asset-filters.service';
 
 @Component({
   selector: 'ang-search-page', 
@@ -16,7 +17,7 @@ export class SearchPage implements OnInit, OnDestroy {
 
   private term: string;
 
-  constructor(private _assets: AssetService, private route: ActivatedRoute) {
+  constructor(private _assets: AssetService, private route: ActivatedRoute, private _filters: AssetFiltersService) {
 
   }
 
@@ -24,6 +25,7 @@ export class SearchPage implements OnInit, OnDestroy {
     // Subscribe to term in params
     this.subscriptions.push(
       this.route.params.subscribe((routeParams) => {
+        this._filters.clearApplied();
         this.term = routeParams["term"];
         if (this.term) {
           this._assets.queryAll(routeParams);
