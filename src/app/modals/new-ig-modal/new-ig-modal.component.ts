3e7164602aa5eea479b02068c40e9526f1a01457
Corbfon <corbin.fonville@gmail.com>
2017-02-23 12:02:19 -0600

    switch ig public permissions options on for artstor users

--- src/app/modals/new-ig-modal/new-ig-modal.component.ts
+++ src/app/modals/new-ig-modal/new-ig-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { formGroupNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
+import { AuthService } from './../../shared';
+
 @Component({
   selector: 'ang-new-ig-modal',
   templateUrl: 'new-ig-modal.component.html'
@@ -10,14 +12,14 @@ export class NewIgModal implements OnInit {
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
 
   private newIgForm: FormGroup;
-  private isArtstorUser: boolean = true;
+  private isArtstorUser: boolean = false;
   private tags: string[] = [];
   // We need to seed the medium editor with an empty div to fix line return issues in Firefox!
   private igDescription: string = "<div>&nbsp;</div>";
 
   private isLoading: boolean = false;
 
-  constructor(_fb: FormBuilder) {
+  constructor(private _auth: AuthService, _fb: FormBuilder) {
     this.newIgForm = _fb.group({
       title: [null, Validators.required],
       artstorPermissions: [this.isArtstorUser ? "private" : null],
@@ -26,7 +28,9 @@ export class NewIgModal implements OnInit {
     })
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.isArtstorUser = this._auth.getUser().institutionId == 24615;
+  }
 
   private igFormSubmit(formValue: any): void {
     console.log(formValue);
