d8017d43bfa583173265bd5336810dc2e9a2be88
Corbfon <corbin.fonville@gmail.com>
2017-02-24 15:51:12 -0600

    add the rest of the copy

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -27,6 +27,7 @@ export class RegisterComponent implements OnInit {
       passwordConfirm: [null, Validators.required],
       role: [null, Validators.required],
       dept: [null, Validators.required],
+      age: [true, Validators.requiredTrue],
       info: true,
       survey: true
     }, { validator: Validators.compose([ this.passwordsEqual, this.emailsEqual ])});
