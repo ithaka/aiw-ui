991538ee709474b3c0b9401051279a81a1d5a1ee
Corbfon <corbin.fonville@gmail.com>
2017-03-06 11:28:46 -0600

    subscribe to current selection

--- src/app/modals/add-to-group/add-to-group.component.ts
+++ src/app/modals/add-to-group/add-to-group.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
+
+import { AssetService } from './../../shared';
 
 @Component({
   selector: 'ang-add-to-group',
   templateUrl: 'add-to-group.component.html'
 })
-export class AddToGroupModal implements OnInit {
+export class AddToGroupModal implements OnInit, OnDestroy {
   @Output() closeModal: EventEmitter<any> = new EventEmitter();
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  private selectedAssets: any[] = [];
 
-  ngOnInit() { }
+  constructor(
+    private _assets: AssetService
+  ) { }
+
+  ngOnInit() {
+    // Subscribe to asset selection
+    this.subscriptions.push(
+      this._assets.selection.subscribe(
+        assets => {
+          this.selectedAssets = assets;
+        },
+        error => {
+          console.log(error);
+        }
+      )
+    );
+  }
+
+  ngOnDestroy() {
+      this.subscriptions.forEach((sub) => { sub.unsubscribe(); });
+  }
 }
