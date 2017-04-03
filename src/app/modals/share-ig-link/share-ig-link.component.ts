4ae5aef3440ff3fc2702311e7f8e598cb3a2bd06
Corbfon <corbin.fonville@gmail.com>
2017-04-03 13:25:42 -0500

    init share ig link modal

---
+++ src/app/modals/share-ig-link/share-ig-link.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'ang-share-ig-link',
+  templateUrl: 'share-ig-link.component.html'
+})
+export class ShareIgLinkModal implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
