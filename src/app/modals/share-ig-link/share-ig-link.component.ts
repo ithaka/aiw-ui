aaf1706641982145c1be06d8f6b7bc089ab26d27
Corbfon <corbin.fonville@gmail.com>
2017-04-04 13:32:51 -0500

    add copying to url and hyperlink fields

--- src/app/modals/share-ig-link/share-ig-link.component.ts
+++ src/app/modals/share-ig-link/share-ig-link.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'ang-share-ig-link',
-  templateUrl: 'share-ig-link.component.html'
+  templateUrl: 'share-ig-link.component.html',
+  styleUrls: ['./share-ig-link.component.scss']
 })
 export class ShareIgLinkModal implements OnInit {
+  @Output()
+  private closeModal: EventEmitter<any> = new EventEmitter();
+
+  private document = document;
+
+  private shareLink: string = "test"; // this is the url which will be copied to the user's clipboard
+  private shareHyperlink: string = "this is another test"; // this is the hyperlink which will be copied to the user's clipboard
+
   constructor() { }
 
   ngOnInit() { }
