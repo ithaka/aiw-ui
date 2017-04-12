026e36bc69fdcc5c50bc5b3227b3cfa2cefadaae
codiak <codypumper@gmail.com>
2017-04-12 08:55:35 -0500

    Fix: Occasional issuing pushing a group

--- src/app/shared/group.service.ts
+++ src/app/shared/group.service.ts
@@ -114,6 +114,7 @@ export class GroupService {
         delete putGroup['public']
         delete putGroup['count']
         delete putGroup['thumbnails']
+        delete putGroup['igDownloadInfo']
         
         if (!putGroup.tags || putGroup.tags[0] == null) { putGroup.tags = [] }
 
