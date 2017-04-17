a2bd3b6d77c9124b45c63cb9ab3f18b1fec17963
Corbfon <corbin.fonville@gmail.com>
2017-04-17 11:12:53 -0500

    handle legacy browse collections routes

--- src/app/legacy.service.ts
+++ src/app/legacy.service.ts
@@ -15,27 +15,29 @@ export class LegacyRouteResolver implements Resolve<boolean> {
     } else {
       let routeNum = urlArr[0].substr(0, 2)
 
+      urlArr.forEach((value, index) => {
+        urlArr[index] = decodeURIComponent(value)
+      })
+
+      console.log(urlArr)
+
+      let pipeArr = urlArr[0].split("|")
+      console.log(pipeArr)
+
       switch (routeNum) {
         case "#2": // handles all of the #2 routes
           console.log("i got #2")
+          this._router.navigate(["/browse", "library", { viewId: pipeArr[1] }])
           break
         case "#3": // handles all of the #3 routes
           console.log("i got #3")
           // #3 routes are usually an array split by the pipe symbol, and the key is the second value in that array (index 1)
-          urlArr.forEach((value, index) => {
-            urlArr[index] = decodeURIComponent(value)
-          })
-
-          console.log(urlArr)
-
-          let pipeArr = urlArr[0].split("|")
-          console.log(pipeArr)
 
           if (pipeArr && pipeArr.length > 0) {
             switch (pipeArr[1]) {
               case "collections":
                 console.log("routing to collections")
-                this._router.navigate(["/collection", pipeArr[2]])
+                this._router.navigate(["/collection", pipeArr[2]]) // the 3rd item in the array is the collection id
                 break
               default:
                 console.log("got the default case")
