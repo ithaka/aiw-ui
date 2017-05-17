a9454f7b1db8d9fbab6a92426578443c138974f3
Corbfon <corbin.fonville@gmail.com>
2017-05-17 15:40:49 -0500

    you won't believe this one weird trick

--- src/app/modals/new-ig-modal/new-ig-modal.component.spec.ts
+++ src/app/modals/new-ig-modal/new-ig-modal.component.spec.ts
@@ -1,41 +1,41 @@
-import { NewIgModal } from './new-ig-modal.component';
-import { BehaviorSubject, Observable } from 'rxjs/Rx';
+import { NewIgModal } from './new-ig-modal.component'
+import { BehaviorSubject, Observable } from 'rxjs/Rx'
 
 describe('NewIgModal', () => {
-    let newIgModal: NewIgModal;
-    let mockFormBuilder: any;
-    let mockAuth: any;
-    let mockAssets: any;
+    let newIgModal: NewIgModal
+    let mockFormBuilder: any
+    let mockAuth: any
+    let mockAssets: any
 
     beforeEach(() => {
         mockFormBuilder = {
-            group: () => { return {}; }
-        };
+            group: () => { return {} }
+        }
         mockAuth = {
             getUser: () => { return  {
                     institutionId: 24615
                 }
             }
-        };
+        }
         mockAssets = {
             selection: new BehaviorSubject([]).asObservable()
-        };
-        newIgModal = new NewIgModal(mockAssets, mockAuth, mockFormBuilder, null, null);
+        }
+        newIgModal = new NewIgModal(mockAssets, mockAuth, mockFormBuilder, null, null)
         // Initialize
-        newIgModal.ngOnInit();
-    });
+        newIgModal.ngOnInit()
+    })
 
     // it('should initialize with an empty tags array', () => {
-    //     expect(newIgModal.tags.length).toBe(0);
-    // });
+    //     expect(newIgModal.tags.length).toBe(0)
+    // })
 
     // it('have a form submission function that toggles "submitted"', () => {
-    //     newIgModal.igFormSubmit({});
-    //     expect(newIgModal.submitted).toBe(true);
-    // });
+    //     newIgModal.igFormSubmit({})
+    //     expect(newIgModal.submitted).toBe(true)
+    // })
 
     // it('should recognize an Artstor institution user', () => {
-    //     // newIgModal.igFormSubmit({});
-    //     expect(newIgModal.isArtstorUser).toBe(true);
-    // });
+    //     // newIgModal.igFormSubmit({})
+    //     expect(newIgModal.isArtstorUser).toBe(true)
+    // })
 });
