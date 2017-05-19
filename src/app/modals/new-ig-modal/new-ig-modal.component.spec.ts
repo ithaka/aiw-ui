5fe3b387a8e273babb151e9a101b1746bdff6b8d
Corbfon <corbin.fonville@gmail.com>
2017-05-19 08:22:03 -0500

    second test and more test upgrades

--- src/app/modals/new-ig-modal/new-ig-modal.component.spec.ts
+++ src/app/modals/new-ig-modal/new-ig-modal.component.spec.ts
@@ -2,7 +2,7 @@
 import { IgFormUtil, IgFormValue } from './new-ig'
 import { BehaviorSubject, Observable } from 'rxjs/Rx'
 
-describe('IgFormUtil', () => {
+fdescribe('IgFormUtil', () => {
     // let newIgModal: NewIgModal
     // let mockFormBuilder: any
     // let mockAuth: any
@@ -30,29 +30,50 @@ describe('IgFormUtil', () => {
     })
 
     describe("prepareGroup", () => {
+        let description = "this is a string"
+        let mockAssets = [{objectId: "12345"}, {objectId: "67890"}]
+        let mockUser = { institutionId: 1234567 }
+
         it("should correctly process a private image group", () => {
 
             let form: IgFormValue = {
-                title: "Test Ig",
+                title: "Private Test Ig",
                 artstorPermissions: "private", // this can be global, institution or private
-                institutionView: false,
+                institutionView: null,
                 tags: ["tag1", "tag2"]
             }
-            let description = "this is a string"
-            let mockAssets = [{objectId: "12345"}, {objectId: "67890"}]
 
-            let group = util.prepareGroup(form, description, mockAssets, { institutionId: 1234567 })
+
+            let group = util.prepareGroup(form, description, mockAssets, mockUser)
             
+            expect(group.id).toBeFalsy() // negative test
+
             // basic assertions pertaining to each field that is required for group creation
             expect(group.name).toBe(form.title)
             expect(group.access.length).toBe(1)
+            expect(group.public).toBeFalsy()
             expect(group.description).toBe(description)
             expect(group.items.length).toBe(2)
             expect(group.items.indexOf(mockAssets[0].objectId)).toBeGreaterThan(-1)
+            expect(group.tags.length).toBe(2)
         })
 
-        // it("should correctly process a global image group", () => {
+        it("should correctly process a global image group", () => {
+            let form: IgFormValue = {
+                title: "Institution Test Ig",
+                artstorPermissions: null,
+                institutionView: true,
+                tags: ["tag1", "tag2"]
+            }
 
-        // })
+            let group = util.prepareGroup(form, description, mockAssets, mockUser)
+
+            expect(group.id).toBeFalsy() // negative test
+
+            expect(group.access.length).toBe(2)
+                expect(group.access[1].entity_type).toBe(200)
+                expect(group.access[1].entity_identifier).toBe(mockUser.institutionId)
+            expect(group.public).toBeFalsy()
+        })
     })
 })
