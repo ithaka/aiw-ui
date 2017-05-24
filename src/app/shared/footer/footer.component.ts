0f6130799f78a7cf964d6cc7af7fff40e8c19448
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2017-05-24 16:39:04 +0500

    Unable to logout on mobile! Fixed

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { LoginService } from '../../login/login.service';
 
 // Project Dependencies
 const { version: appVersion } = require('../../../../package.json');
@@ -6,7 +10,7 @@ const { version: appVersion } = require('../../../../package.json');
 @Component({
   selector: 'footer',
   providers: [
-    
+    LoginService
   ],
   templateUrl: './footer.component.html',
   styleUrls: [ './footer.component.scss' ],
@@ -16,7 +20,12 @@ export class Footer {
   private currentYear;
   
   // TypeScript public modifiers
-  constructor( ) { 
+  constructor( 
+    private location: Location,
+    private _router: Router,
+    private _login: LoginService
+
+  ) { 
     // Get version number
     this.appVersion = appVersion;
 
@@ -27,5 +36,16 @@ export class Footer {
   
   ngOnInit() {
     
+  }
+
+  private logout(): void {
+    this._login.logout()
+      .then(() => {
+        if (this.location.path().indexOf("home") >= 0) {
+          location.reload() // this will reload the app and give the user a feeling they actually logged out
+        } else {
+          this._router.navigate(['/home'])
+        }
+      })
   }
 }
