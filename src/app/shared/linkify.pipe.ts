d032b23e6816103385e3f448f7f8a553d96a4b0d
codiak <codypumper@gmail.com>
2017-06-12 07:28:07 -0500

    Linkify links into new window

--- src/app/shared/linkify.pipe.ts
+++ src/app/shared/linkify.pipe.ts
@@ -1,11 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-// import linkifyStr from 'linkifyjs/string';
+// import { linkifyStr } from 'linkifyjs/string';
 
 var linkifyStr = require('linkifyjs/string');
 
 @Pipe({name: 'linkify'})
 export class LinkifyPipe implements PipeTransform {
   transform(str: string): string {
-    return str ? linkifyStr(str, {target: '_system'}) : str;
+    return str ? linkifyStr(str, {target: '_blank'}) : str;
   }
 }
