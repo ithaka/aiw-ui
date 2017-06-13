0389c2f7e51b6b2c433bf5649ba076e880eeba37
Corbfon <corbin.fonville@gmail.com>
2017-06-13 14:10:07 -0500

    abstract the tags list into its own component

--- src/app/browse-page/browse-groups/tags-list.component.ts
+++ src/app/browse-page/browse-groups/tags-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ang-tags-list',
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TagsListComponent implements OnInit {
+  @Output() clearFilters: EventEmitter<any> = new EventEmitter()
+  @Output() selectTag: EventEmitter<string> = new EventEmitter()
+
+  @Input() tagFilters: any[] = []
+  @Input() appliedTags: any[] = []
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    console.log(this.tagFilters)
+  }
 }
