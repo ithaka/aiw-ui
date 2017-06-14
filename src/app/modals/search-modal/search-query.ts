ebb7703d099fa2b45fcfb08f69864943d83daab7
codiak <codypumper@gmail.com>
2017-06-14 17:02:29 -0500

    Advance search field queries

--- src/app/modals/search-modal/search-query.ts
+++ src/app/modals/search-modal/search-query.ts
@@ -20,10 +20,10 @@ export class SearchQueryUtil {
       }
       
       if (index !== 0) {
-        advQuery += "#" + query.operator.toLowerCase() + ","
+        advQuery += " " + query.operator.toUpperCase() + " "
       }
-
-      advQuery += query.term + '|' + query.field.value
+      
+      advQuery += query.field.value + ':' + "\"" + query.term + "\""
     })
     
     if (advQuery.length < 1) {
