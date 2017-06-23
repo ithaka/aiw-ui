872979ef6c2662534eedb161a08e321304607fe2
codiak <codypumper@gmail.com>
2017-06-23 14:30:36 -0500

    Update 404 page

--- src/app/no-content/no-content.ts
+++ src/app/no-content/no-content.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'no-content',
   template: `
-    <div>
-      <h1>404: page missing</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2 pb-4">
+          <img class="img-fluid py-4" src="/assets/img/404.png" />
+          <h1>Page Not Found</h1>
+          <p>We couldn't find what you were looking for!</p>
+        </div>
+      </div>
     </div>
   `
 })
