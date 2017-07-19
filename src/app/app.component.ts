129ac372600612550fb5189e36e21b886188f7d7
codiak <codypumper@gmail.com>
2017-07-19 17:19:08 -0400

    sr skip update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ import { AppState } from './app.service';
     '../sass/app.scss'
   ],
   template: `
+    <a href="#mainContent" class="sr-only sr-only-focusable">Skip to main content</a>
     <ang-sky-banner *ngIf="showSkyBanner" [textValue]="'BETA_SKY_BANNER.MESSAGE' | translate" (closeBanner)="showSkyBanner = false"></ang-sky-banner>
     <nav-bar></nav-bar>
 
