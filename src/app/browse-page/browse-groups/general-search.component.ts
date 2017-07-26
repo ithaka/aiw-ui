2591cb97148a4f79402c9a7351c4ba7a2057df15
Corbfon <corbin.fonville@gmail.com>
2017-07-26 13:39:33 -0500

    add ability to change search term to general search component

--- src/app/browse-page/browse-groups/general-search.component.ts
+++ src/app/browse-page/browse-groups/general-search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription'
 
 @Component({
   selector: 'ang-general-search',
@@ -7,12 +8,23 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 export class GeneralSearchComponent implements OnInit {
   @Output() executeSearch: EventEmitter<string> = new EventEmitter()
+  @Input() updateSearchTerm: EventEmitter<string> = new EventEmitter() // allows an outside component to set the search term
+
+  private subscriptions: Subscription[] = []
 
   // the term that will be searched for
   private term: string = ''
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    if (this.updateSearchTerm) {
+      this.subscriptions.push(
+        this.updateSearchTerm.subscribe((term) => {
+          this.term = term
+        })
+      )
+    }
+  }
 
 }
