e63cef93daa563dd666a25d4a1a73c3009ff0e4e
codiak <codypumper@gmail.com>
2017-08-07 11:25:46 -0500

    Fix: Search flag additional param filter

--- src/app/shared/asset-search.service.ts
+++ src/app/shared/asset-search.service.ts
@@ -79,7 +79,7 @@ export class AssetSearchService {
     for (var i = 0; i < filters.length; i++) { // Applied filters
       
       
-      if (filters[i].filterGroup === 'collTypes' || filters[i].filterGroup === 'currentPage' || filters[i].filterGroup === 'pageSize') { 
+      if ( ['collTypes', 'currentPage', 'pageSize', 'sort'].indexOf(filters[i].filterGroup) > -1) { 
         // Collection Types and page info
         // do nothing
         // colTypeIds = filters[i].filterValue;
