d48ab6e526ab9b0f652e6d2652fffb24eef990d7
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2017-09-20 17:10:38 +0500

    SOLR search - Institutional Collections filter label should be replaced with institution shortName, if available!

--- src/app/asset-filters/asset-filters.service.ts
+++ src/app/asset-filters/asset-filters.service.ts
@@ -63,12 +63,12 @@ export class AssetFiltersService {
     private institution: any = {};
 
 
-    private readonly filterNameMap: any = {
+    private filterNameMap: any = {
         "collectiontypes" : {
         1 : "Artstor Digital Library",
-        2 : "Institution Collections",
+        // 2 : "Institution Collections",
         3 : "Private Collections",
-        4 : "Institution Collections",
+        // 4 : "Institution Collections",
         5 : "Open Collections",
         6 : "Private Collections"
         }
@@ -81,6 +81,8 @@ export class AssetFiltersService {
     ){
         this._storage = locker.useDriver(Locker.DRIVERS.LOCAL);
         this.institution = this._storage.get('institution');
+        this.filterNameMap["collectiontypes"][2] = this.filterNameMap["collectiontypes"][4] = this.institution && this.institution.shortName ? this.institution.shortName + ' Collections' : 'Institution Collections';
+        // console.log(this.institution.shortName);
     }
 
     public getFilterNameMap() : any {
