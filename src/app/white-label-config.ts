13fe1ba37fa5c9f3d9053568496a4decec032b60
Corbfon <corbin.fonville@gmail.com>
2017-10-05 15:03:08 -0500

    change how we're managing links in WLV file

--- src/app/white-label-config.ts
+++ src/app/white-label-config.ts
@@ -9,19 +9,22 @@ export const WLV_ARTSTOR = {
     pageTitle: "Artstor",
     logoUrl: "/assets/img/logo-v1-1.png",
     footerLinks: [
-        "<a href=\"http://www.artstor.org/content/artstor-digital-library-features-benefits\" class=\"link\" target=\"_blank\">About</a>",
-        "<a href=\"http://support.artstor.org/?page_id=52\" class=\"link\" target=\"_blank\">Getting Started</a>",
-        "<a href=\"http://www.artstor.org/content/teaching-resources-overview\" class=\"link\" target=\"_blank\">Teaching and Study Ideas</a>",
-        "<a href=\"http://www.artstor.org/content/join-our-mailing-list\" class=\"link\" target=\"_blank\">Stay Informed</a>",
-        "<a href=\"http://www.artstor.org/terms\" class=\"link\" target=\"_blank\">Terms</a>",
-        "<a href=\"http://www.artstor.org/privacy-policy\" class=\"link\" target=\"_blank\">Privacy</a>",
-        "<a href=\"http://www.artstor.org/copyright\" class=\"link\" target=\"_blank\">Copyright Concerns</a>",
-        "<a href=\"http://support.artstor.org\" class=\"link\" target=\"_blank\">Support</a>"
+        "ABOUT",
+        "GETTING_STARTED",
+        "TEACHING_IDEAS",
+        "STAY_INFORMED",
+        "TERMS",
+        "PRIVACY",
+        "COPYRIGHT",
+        "SUPPORT"
     ]
 }
 
 export const WLV_SAHARA = {
     "logoUrl" : "/assets/img/logo-sahara-v1.png",
     "institutionLogin" : false,
-    "pageTitle" : "SAHARA"
+    "pageTitle" : "SAHARA",
+    footerLinks: [
+
+    ]
 }
