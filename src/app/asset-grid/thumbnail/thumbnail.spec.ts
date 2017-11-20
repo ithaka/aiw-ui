1da750ac2f2f5ae576709096a896a1120678d9c1
Robert Niznik <rniznik@ithaa1rniznik.office.share.org>
2017-11-20 15:57:05 -0500

    correction

--- src/app/asset-grid/thumbnail/thumbnail.spec.ts
+++ src/app/asset-grid/thumbnail/thumbnail.spec.ts
@@ -1,5 +1,5 @@
 // test imports
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 
@@ -12,45 +12,44 @@ import { AssetService } from './../../shared/assets.service';
 import { TypeIdPipe } from './../../shared';
 import { Thumbnail } from './../../shared'
 
+class MockTestService {
+  public makeThumbUrl(imagePath: string, size ?: number): string {
+    return 'fakeURL';
+  }
+}
+
 fdescribe('Thumbnail', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ThumbnailComponent, TypeIdPipe ], // declare the test component
       providers: [
         { provide: Router, useValue: {} },
-        { provide: AssetService, useValue: {} }
+        { provide: AssetService, useValue: {}, useClass: MockTestService }
       ]
     })
     .compileComponents();  // compile template and css
   }));
 
   describe("Thumbnail component", () => {
-    let thumbnail: ThumbnailComponent;
+    let comp: ThumbnailComponent;
     let fixture: ComponentFixture<ThumbnailComponent>;
     let de: DebugElement;
     let el: HTMLElement;
 
     beforeEach(() => {
       fixture = TestBed.createComponent(ThumbnailComponent);
-
-      thumbnail = fixture.componentInstance;
+      comp = fixture.componentInstance;
     });
 
-    it("should have alt text with value of asset title", () => {
+    it("should have alt text with value of asset title", inject([AssetService], (_assets: AssetService) => {
       // initial test to verify that alt text exists
-      let thumbnail = { arttitle: ['title'], thumbnail1: 'test-title' }
+      (comp as any).thumbnail = { arttitle: ['title'], thumbnail1: 'test-title', thumbnailImgUrl: 'fake' }
+      fixture.detectChanges();
+
       de = fixture.debugElement.query(By.css('img'));
       el = de.nativeElement;
-      el.setAttribute('alt', 'Thumbnail of ' + thumbnail.thumbnail1);
+      console.log(el);
       expect(el.attributes['alt'].value).toBe('Thumbnail of test-title');
-    });
-
-    it("should have a tabindex", () => {
-      // initial test to verify that tabindex exists
-      de = fixture.debugElement.query(By.css('.card-block.card-text'));
-      el = de.nativeElement;
-      el.setAttribute('tabindex', '1');
-      expect(el.attributes['tabindex'].value).toBe('1');
-    });
+    }));
   })
 });
