b350b05ffb458ce922d1e83d3b2d5d5bbe5d0d8d
Corbfon <corbin.fonville@gmail.com>
2017-12-15 15:30:44 -0600

    wire up the logs when you click an asset

--- src/app/shared/log.service.ts
+++ src/app/shared/log.service.ts
@@ -10,21 +10,21 @@ export class LogService {
     private _auth: AuthService
   ) { }
 
-  // public log(message: any): void { // message is only typed as any because we want to log anything that is sent
-  //   this._http.post<LogResponse>(
-  //     [this._auth.getUrl(), 'v1', 'log'].join('/'),
-  //     message,
-  //     { withCredentials: true }
-  //   )
-  //   .take(1)
-  //   .subscribe((res) => {
-  //     console.log(res)
-  //   }, (err) => {
-  //     console.error(err)
-  //   })
-  // }
+  public log(message: any): void { // message is only typed as any because we want to log anything that is sent
+    this._http.post<LogResponse>(
+      [this._auth.getUrl(), 'v1', 'log'].join('/'),
+      message,
+      { withCredentials: true }
+    )
+    .take(1)
+    .subscribe((res) => {
+      console.log(res)
+    }, (err) => {
+      console.error(err)
+    })
+  }
 }
 
-// interface LogResponse {
+interface LogResponse {
 
-// }
+}
