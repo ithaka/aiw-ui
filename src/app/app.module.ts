e434bea00f43869af0de1c489bb6c4a1625111b8
Corbfon <corbin.fonville@gmail.com>
2017-12-15 15:23:39 -0600

    add log service and provide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -82,21 +82,29 @@ import {
   SessionExpireModal,
   RegisterJstorModal
 } from './modals';
-import { TitleService } from './shared/title.service'
 import { GeneralSearchComponent } from './browse-page/browse-groups/general-search.component'
 import { SkyBannerComponent } from './sky-banner/sky-banner.component'
 
 
 // Application wide providers
-import { AuthService, AssetService, GroupService, TypeIdPipe, ToolboxService, ImageGroupService } from './shared';
-import { AssetFiltersService } from './asset-filters/asset-filters.service';
-import { TagsService } from './browse-page/tags.service';
-import { CustomReuseStrategy } from './reuse-strategy';
-import { LegacyRouteResolver } from './legacy.service';
-import { AnalyticsService } from './analytics.service';
+import {
+  AuthService,
+  AssetService,
+  GroupService,
+  ImageGroupService,
+  LogService,
+  TitleService,
+  ToolboxService,
+  TypeIdPipe,
+} from './shared'
+import { AssetFiltersService } from './asset-filters/asset-filters.service'
+import { TagsService } from './browse-page/tags.service'
+import { CustomReuseStrategy } from './reuse-strategy'
+import { LegacyRouteResolver } from './legacy.service'
+import { AnalyticsService } from './analytics.service'
 
-import { LinkifyPipe } from './shared/linkify.pipe';
-import { CustomUrlSerializer } from './shared/custom-url-serializer';
+import { LinkifyPipe } from './shared/linkify.pipe'
+import { CustomUrlSerializer } from './shared/custom-url-serializer'
 
 
 const APP_PROVIDERS = [
@@ -108,6 +116,7 @@ const APP_PROVIDERS = [
   AssetSearchService,
   GroupService,
   AuthService,
+  LogService,
   ImageGroupService,
   AssetFiltersService,
   TagFiltersService,
