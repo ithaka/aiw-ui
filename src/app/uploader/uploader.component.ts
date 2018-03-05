ac99e63ccb8e0f8c3bd787c1b005cb780e1fec64
Corbfon <corbin.fonville@gmail.com>
2018-03-05 13:13:23 -0600

    merge fix

---
+++ src/app/uploader/uploader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ang-uploader',
+  templateUrl: 'uploader.component.pug'
+})
+
+export class UploaderComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
