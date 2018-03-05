52a1e0c457a22035c365bcc124fba1c7cfa7e25f
Corbfon <corbin.fonville@gmail.com>
2018-03-05 13:20:38 -0600

    add uploader to edit personal collection modal

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+import { FileUploader, FileSelectDirective, FileLikeObject } from 'ng2-file-upload'
 
 @Component({
   selector: 'ang-uploader',
-  templateUrl: 'uploader.component.pug'
+  templateUrl: 'uploader.component.pug',
+  styleUrls: ['./uploader.component.scss']
 })
 
 export class UploaderComponent implements OnInit {
+
+  private uploadURL: string = 'nothing'
+  private uploader:FileUploader = new FileUploader({ url: this.uploadURL, allowedMimeType:["image/JPEG", "image/JPG", "image/jpeg", "image/jpg"] })
+
   constructor() { }
 
   ngOnInit() { }
