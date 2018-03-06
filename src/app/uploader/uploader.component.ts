3fb620a06d3aa291d4bdefa09770d54341fbbdd8
Corbfon <corbin.fonville@gmail.com>
2018-03-06 15:44:43 -0600

    files finnnnnnally uploading

--- src/app/uploader/uploader.component.ts
+++ src/app/uploader/uploader.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 
 import { FileUploader, FileSelectDirective, FileLikeObject } from 'ng2-file-upload'
+import { AuthService } from '../shared'
 
-const UPLOAD_URL: string = 'https://evening-anchorage-3159.herokuapp.com/api/' //TODO: NEEDS CHANGING - JUST AND EXPERIMENT
+// const UPLOAD_URL: string = 'https://evening-anchorage-3159.herokuapp.com/api/' //TODO: NEEDS CHANGING - JUST AND EXPERIMENT
 
 @Component({
   selector: 'ang-uploader',
@@ -11,14 +12,23 @@ const UPLOAD_URL: string = 'https://evening-anchorage-3159.herokuapp.com/api/' /
 })
 
 export class UploaderComponent implements OnInit {
-  private uploader: FileUploader = new FileUploader({ url: UPLOAD_URL, allowedMimeType:["image/JPEG", "image/JPG", "image/jpeg", "image/jpg"] })
+  private uploader: FileUploader
+  private UPLOAD_URL: string
 
   // controls display of drop zone while there is a file over it
   private fileOverDropZone: boolean = false
 
   private invalidFiles: FileLikeObject[] = []
 
-  constructor() { }
+  constructor(private _auth: AuthService) {
+    this.UPLOAD_URL = [this._auth.getUrl(), 'v1', 'pcollection', 'image'].join('/')
+
+    this.uploader = new FileUploader({
+      url: this.UPLOAD_URL,
+      allowedMimeType:["image/JPEG", "image/JPG", "image/jpeg", "image/jpg"],
+      autoUpload: true
+    })
+  }
 
   ngOnInit() {
     this.uploader.onWhenAddingFileFailed = (item, filter, options) => {
@@ -26,7 +36,7 @@ export class UploaderComponent implements OnInit {
 
       // console.log(item, filter, options)
       this.invalidFiles.push(item)
-      console.log(this.invalidFiles)
+      // console.log(this.invalidFiles)
       // this.typeValid = false;
       setTimeout(() => {
         // this.typeValid = true;
@@ -48,6 +58,9 @@ export class UploaderComponent implements OnInit {
    */
   private fileDropped(files: RawFile[]): void {
     // this.droppedFiles.concat(files)
+    setTimeout(()=> {
+      console.log(this.uploader.queue)
+    }, 1000)
   }
 }
 
