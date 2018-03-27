37bef6f5f391661778ed38ed7bedf9eae86b7808
Corbfon <corbin.fonville@gmail.com>
2018-03-27 08:54:14 -0500

    stop using objectTypeId in favor of adlObjectType

--- src/app/shared/datatypes/thumbnail.interface.ts
+++ src/app/shared/datatypes/thumbnail.interface.ts
@@ -2,20 +2,22 @@
 //  if you need more, you can add them to the interface
 
 export interface Thumbnail {
-  objectId: string;
-  objectTypeId: number;
-  collectionId: string;
-  collectionType: number;
-  cfObjCount: number; // number of associated assets
-  cfObjectId: string; // haven't seen any for which this is different than objectId
-  count: number;
-  largeImageUrl: string;
-  publicAccess: boolean;
-  iapFlag?: number;
-  thumbnail1: any;
-  thumbnail2: any;
-  thumbnail3: any;
-  thumbnail4: any;
+  objectId: string
+  collectionId: string
+  collectionType: number
+  cfObjCount: number // number of associated assets
+  cfObjectId: string // haven't seen any for which this is different than objectId
+  count: number
+  largeImageUrl: string
+  publicAccess: boolean
+  iapFlag?: number
+  media: {
+    adlObjectType: string
+  }
+  thumbnail1: any
+  thumbnail2: any
+  thumbnail3: any
+  thumbnail4: any
 }
 
 // export interface SearchThumbnail {
