7a7cd24c2cc6bb0ac8c6dbb06771506a87fd5b3c
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-03-27 18:19:49 +0500

    Load PC assets via SOLR

--- src/app/shared/asset-search.service.ts
+++ src/app/shared/asset-search.service.ts
@@ -259,13 +259,16 @@ export class AssetSearchService {
       }
     }
 
-    if(options.colId || options['coll']){
-      let colId = '';
-      if( options['coll'] ){
-        colId = options['coll'];
+    if(options.colId || options['coll'] || options.pcolId){
+      let colId = ''
+      if(options['coll']){
+        colId = options['coll']
       }
-      else if ( options.colId ){
-        colId = options.colId;
+      else if(options.colId){
+        colId = options.colId
+      }
+      else if(options.pcolId){
+        colId = '37436'
       }
 
       filterArray.push("collections:\"" + colId + "\"");
@@ -606,4 +609,5 @@ interface SearchOptions {
   size?: number
   colId?: string
   collections?: string
+  pcolId?: string
 }
