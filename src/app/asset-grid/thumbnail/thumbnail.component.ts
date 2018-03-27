1f4ecc59b5781c54c2bb07c5034969648ce6a45d
Corbfon <corbin.fonville@gmail.com>
2018-03-27 08:54:14 -0500

    stop using objectTypeId in favor of adlObjectType

--- src/app/asset-grid/thumbnail/thumbnail.component.ts
+++ src/app/asset-grid/thumbnail/thumbnail.component.ts
@@ -42,6 +42,8 @@ export class ThumbnailComponent implements OnInit, OnChanges {
    }
 
   ngOnInit() {
+    console.log(this.thumbnail)
+
     if (this.thumbnail['media']) {
       // let media = JSON.parse(this.thumbnail['media'])
       this.thumbnail['thumbnailImgUrl'] = this.thumbnail['media']['thumbnailSizeOnePath']
