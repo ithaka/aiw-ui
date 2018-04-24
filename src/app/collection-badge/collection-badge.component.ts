8c4c6e2c165a299156b898bad216695819f0855a
Corbfon <corbin.fonville@gmail.com>
2018-04-24 08:15:46 -0500

    init collection badge

---
+++ src/app/collection-badge/collection-badge.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ang-collection-badge',
+  templateUrl: 'collection-badge.component.pug'
+})
+
+export class CollectionBadgeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
