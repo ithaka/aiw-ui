5fdd5ef06c86c4787fc0d8a044911d978a937971
Corbfon <corbin.fonville@gmail.com>
2018-04-25 11:24:52 -0500

    add badgeText to collection type handler

--- src/app/asset-grid/thumbnail/thumbnail.component.ts
+++ src/app/asset-grid/thumbnail/thumbnail.component.ts
@@ -1,7 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 
-import { Thumbnail, AssetService, CollectionTypeHandler, AssetSearchService } from './../../shared'
+import { Thumbnail, AssetService, CollectionTypeHandler, AssetSearchService, CollectionTypeInfo } from './../../shared'
 
 @Component({
   selector: 'ang-thumbnail',
@@ -81,7 +81,7 @@ export class ThumbnailComponent implements OnInit, OnChanges {
   }
 
   // wrapper function for getting the collection type
-  getCollectionType(): { name: string, alt: string } {
+  getCollectionType(): CollectionTypeInfo {
     // Some endpoints give us the collectionType info in 'collectionType: number', where as others give the same info in 'collectiontypes: Array<number>'
     return this.collectionTypeHandler.getCollectionType( this.thumbnail['collectionType'] ? [ this.thumbnail['collectionType'] ] : this.thumbnail['collectiontypes'], this.thumbnail['contributinginstitutionid'])
   }
