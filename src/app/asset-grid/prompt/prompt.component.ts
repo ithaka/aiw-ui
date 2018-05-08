e3f0b08ae564796b8e5549cb2465d9ee9fa5b11d
jialinhe96 <jialinhe@umich.edu>
2018-05-08 09:13:58 -0400

    Add the banner to unaffiliated users

--- src/app/asset-grid/prompt/prompt.component.ts
+++ src/app/asset-grid/prompt/prompt.component.ts
@@ -1,98 +1,65 @@
-import { Router } from '@angular/router';
-import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
-import { Thumbnail, AssetService, CollectionTypeHandler, AssetSearchService, CollectionTypeInfo } from './../../shared'
+//import { Thumbnail, AssetService, CollectionTypeHandler, AssetSearchService, CollectionTypeInfo } from './../../shared'
 
 @Component({
   selector: 'ang-prompt',
   templateUrl: 'prompt.component.pug',
   styles: [`
-    .card-icon-group {
-        height: 19px;
-    }
-    .metadata-wrap {
-      min-height: 42px;
-    }
-    .disablePointerEvents{
-        pointer-events: none;
-    }
-    .icon {
-      margin-top: 3px;
-    }
+  .prompt-box {
+    box-shadow: 0px 1px 7px 1px #80808052;
+    padding: 0.8rem;
+    border-radius: 3px;
+    font-size: 0.9rem;
+    line-height: 120%;
+  }
+
+  #promptImg1 {
+    position: relative;
+    right: 8rem;
+    height: 5rem;
+    margin-bottom: -6rem;
+    margin-top: -5rem;
+  }
+
+  #promptImg2 {
+    position: relative;
+    right: 5rem;
+    height: 8rem;
+    margin-top: -13rem;
+    margin-bottom: -9rem;
+  }
+
+  #promptImg3 {
+    position: relative;
+    right: 0rem;
+    height: 7rem;
+    margin-top: -15rem;
+    margin-bottom: -10rem;
+  }
   `]
 })
 export class PromptComponent implements OnInit, OnChanges {
-  // @Input()
-  // private thumbnail: Thumbnail
+  @Input()
+  private searchTerm: string;
 
-  // @Input()
-  // private largeThmbView: boolean
-
-  // @Input()
-  // private reorderMode: boolean
-
-  // @Input()
-  // private editMode: boolean
-
-  private constraints: any = {}
-  private collectionTypeHandler: CollectionTypeHandler = new CollectionTypeHandler()
-
-  // Variable that determines the thumbnail image size based on largeThmbView and available size for the asset. Defaults to 1 (Small thumbnail view)
-  private thumbnailSize: number = 1
-
-  constructor(
-    private _assets: AssetService,
-    private _search: AssetSearchService,
-    private router: Router
-  ) {
-  }
+  private displayWords: string;
 
   ngOnInit() {
-  //   if (this.thumbnail['media']) {
-  //     this.thumbnail.thumbnailImgUrl = this.thumbnail.media.thumbnailSizeOnePath
-  //   }
+    if (window.location.href.indexOf('search') < 1 || this.searchTerm == "*") {
+      this.displayWords = "Search across more than 2 million images in the Artstor Digital Library."
+    }
+    else {
+      this.displayWords = "Search for “" + this.searchTerm + "” across over 2 million assets in Artstor Digital Library."
+    }
+  } 
 
-  //   this.thumbnail.iapFlag = this.determineIAP(this.thumbnail['artstorid'] ? this.thumbnail['artstorid'] : this.thumbnail['objectId'])
+  ngOnChanges() {
+    if (window.location.href.indexOf('search') < 1 || this.searchTerm == "*") {
+      this.displayWords = "Search across more than 2 million images in the Artstor Digital Library."
+    }
+    else {
+      this.displayWords = "Search for “" + this.searchTerm + "” across over 2 million assets in Artstor Digital Library."
+    }
   }
-
-  // Fires when the component input(s) (i.e largeThmbView) changes - Updates the thumbnailSize based on largeThmbView current value
-  ngOnChanges(changes: SimpleChanges) {
-    // if(changes.largeThmbView){
-    //   this.thumbnailSize = changes.largeThmbView.currentValue ? 2 : 1
-    // }
-  }
-
-  openLink(event: Event, urlParams: any[]) {
-    // Prevent the parent anchor tag from firing
-    event.preventDefault()
-    event.stopPropagation()
-
-    this.router.navigate(urlParams)
-  }
-
-  /**
-   * Determine if asset is IAP
-   * @param assetId this should be the asset's id, but if it's undefined, return false
-   */
-  // determineIAP (assetId: string) {
-  //   if (!assetId) {
-  //     return 0
-  //   } else {
-  //      // matches on IAP String, return 1 for true, 0 for false
-  //     return assetId.match(/IAP/) ? 1 : 0
-  //   }
-  // }
-
-  // // wrapper function for getting the collection type
-  // getCollectionType(): CollectionTypeInfo {
-  //   // Some endpoints give us the collectionType info in 'collectionType: number', where as others give the same info in 'collectiontypes: Array<number>'
-  //   return this.collectionTypeHandler.getCollectionType( this.thumbnail['collectionType'] ? [ this.thumbnail['collectionType'] ] : this.thumbnail['collectiontypes'], this.thumbnail['contributinginstitutionid'])
-  // }
-
-  // If large thumbnail image fails to load, fallback to smaller thumbnail image
-  // thumbnailError(): void{
-  //   if(this.thumbnailSize > 1){
-  //     this.thumbnailSize--
-  //   }
-  // }
 }
