b1f4f8821f5da00087d166dbd48624884112db35
bfraley <brett.fraley@ithaka.org>
2018-05-11 15:21:16 -0400

    scaffold featured.component in home.component dir

---
+++ src/app/home/featured/featured.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'featured',
+  templateUrl: 'featured.component.pug',
+  styleUrls: [ './featured.component.scss' ]
+})
+export class FeaturedComponent implements OnInit {
+
+
+  ngOnInit() { }
+
+
+}
