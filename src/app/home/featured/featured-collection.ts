078e49923db3ccf2b061fa5dbc02979398b1606d
bfraley <brett.fraley@ithaka.org>
2018-05-15 14:09:03 -0400

    defines FeaturedCollection type, and do most of our string concats in the component now

---
+++ src/app/home/featured/featured-collection.ts
@@ -0,0 +1,10 @@
+// Interface for featured collections defined in en.json
+
+interface FeaturedCollection {
+    subheading: string;
+    description: string;
+    img_src: string;
+    link: string;
+    link_title: string;
+    alt: string;
+}
