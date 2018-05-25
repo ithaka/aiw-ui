650ea5ecda64a76d6c19ec431e2326dcb3641437
bfraley <brett.fraley@ithaka.org>
2018-05-25 11:03:01 -0400

    var => let

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -488,7 +488,7 @@ export class AuthService implements CanActivate {
       .map(
         (data)  => {
           let user = this.decorateValidUser(data)
-          var onSahara: boolean = this.getHostname().toString().includes('sahara')
+          let onSahara: boolean = this.getHostname().toString().includes('sahara')
           if (user && (!onSahara || user.status)) {
             // Clear expired session modal
             this.showUserInactiveModal.next(false)
