4333d076e255e9a4e46f1be661594dd1c4af082c
bfraley <brett.fraley@ithaka.org>
2018-05-31 14:01:37 -0400

    log in adv search

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -9,7 +9,6 @@ import { AnalyticsService } from '../analytics.service';
 import { AssetGrid } from './../asset-grid/asset-grid.component';
 import { TitleService } from '../shared/title.service';
 import { AppConfig } from '../app.service';
-import { LogService } from '../shared/log.service';
 
 @Component({
   selector: 'ang-search-page',
@@ -39,8 +38,7 @@ export class SearchPage implements OnInit, OnDestroy {
         private _router: Router,
         private _analytics: AnalyticsService,
         private _title: TitleService,
-        private _auth: AuthService,
-        private _captainsLog: LogService
+        private _auth: AuthService
       ) {
     this.siteID = this._appConfig.config.siteID;
     // this makes the window always render scrolled to the top
@@ -87,12 +85,6 @@ export class SearchPage implements OnInit, OnDestroy {
 
         // Make a search call if there is a search term or any selected filter
         if (params["term"] || params["classification"] || params["geography"] || params["collectiontypes"]  || params["collTypes"] || params["startDate"] || params["endDate"]) {
-
-          this._captainsLog.log({
-            eventType: "artstor_search",
-            additional_fields: { searchTerm: params }
-          })
-
           this._title.setSubtitle( '"'+ params["term"] + '"' )
           this._assets.queryAll(params);
         } else {
