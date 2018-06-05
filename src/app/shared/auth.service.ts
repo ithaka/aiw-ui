780bd527225d49ab64ca1378a4e0a60614953c80
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-06-05 17:10:47 +0500

    More robust fix for restricting '/register' route to unaffiliated users

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -501,7 +501,7 @@ export class AuthService implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     let options = { headers: this.userInfoHeader, withCredentials: true }
 
-    if (this.isPublicOnly() && state.url === '/register') { // For unaffiliated users, trying to access /register route
+    if (this.isPublicOnly() && state.url.includes('/register')) { // For unaffiliated users, trying to access /register route
       return new Observable(observer => {
         observer.next(false)
       })
