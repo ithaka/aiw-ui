d8271f113956578255fd06042471b22734b1e3a3
bfraley <brett.fraley@ithaka.org>
2018-06-08 19:59:53 -0400

    cleanup

--- src/app/shared/linkify.pipe.ts
+++ src/app/shared/linkify.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
-// import { linkifyStr } from 'linkifyjs/string';
 
 var linkifyHtml = require('linkifyjs/html');
 
 @Pipe({name: 'linkify'})
 export class LinkifyPipe implements PipeTransform {
   transform(str: string): string {
-    //return str ? linkifyElement(str, {target: '_blank', events:{click:function(e){console.log('Link clicked!');}}}) : str;
     return str ? linkifyHtml(str, {target: '_blank'}) : str;
   }
 }
