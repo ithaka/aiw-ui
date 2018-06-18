9498d8dd05313d86498533bbbf0bd3b9360a240d
Corbfon <corbin.fonville@gmail.com>
2018-06-18 15:23:16 -0500

    compare users country code and set unaffiliated access

--- src/app/shared/flag.service.ts
+++ src/app/shared/flag.service.ts
@@ -9,8 +9,8 @@ export class FlagService {
    * all public properties here are intended to be feature flags which are read/write accessible from any
    *  component or service which imports the FlagService
    */
-  public pcUpload: false
-  public unaffiliated: false
+  public pcUpload: boolean = false
+  public unaffiliated: boolean = false
 
   constructor(
     private _http: HttpClient,
@@ -21,10 +21,40 @@ export class FlagService {
   public getFlagsFromService(): Observable<FlagServiceResponse> {
     const flagUrl: string = [this._injector.get(AuthService).getUrl(), 'flags'].join('/')
     
-    return this._http.get<FlagServiceResponse>(flagUrl)
+    return this._http.get<FlagServiceResponse>(
+      flagUrl,
+      {
+        observe: 'response'
+      }
+    )
+    .map((res) => {
+      let flags = res.body
+
+      // boolean assignments
+      this.unaffiliated = flags.unaffiliatedAccess
+
+      let userCountryCode: string = res.headers.get('x-artstor-country-code')
+      flags.unaffiliatedAccessRollout.forEach((countryCode) => {
+        if (flags.unaffiliatedAccessRollout.indexOf(userCountryCode) > -1) {
+          this.unaffiliated = true
+        }
+      })
+
+      return flags
+    })
+  }
+
+  private setFlagsFromResponse(flags: FlagServiceResponse): void {
+    // boolean assignments
+    this.unaffiliated = flags.unaffiliatedAccess
+
+    flags.unaffiliatedAccessRollout.forEach((countryCode) => {
+
+    })
   }
 }
 
 interface FlagServiceResponse {
-
+  unaffiliatedAccess: boolean
+  unaffiliatedAccessRollout: string[]
 }
