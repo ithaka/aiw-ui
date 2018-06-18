24d55ca56a71d416abf2a42c71eb2fea57f9ce6c
Corbfon <corbin.fonville@gmail.com>
2018-06-18 12:49:01 -0500

    set up call for obtaining flags from flag service

--- src/app/shared/flag.service.ts
+++ src/app/shared/flag.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { AuthService } from './auth.service'
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class FlagService {
@@ -6,9 +9,22 @@ export class FlagService {
    * all public properties here are intended to be feature flags which are read/write accessible from any
    *  component or service which imports the FlagService
    */
-  pcUpload: false
-  unaffiliated: false
+  public pcUpload: false
+  public unaffiliated: false
 
-  constructor() {
+  constructor(
+    private _http: HttpClient,
+    private _injector: Injector
+  ) {
   }
+
+  public getFlagsFromService(): Observable<FlagServiceResponse> {
+    const flagUrl: string = [this._injector.get(AuthService).getUrl(), 'flags'].join('/')
+    
+    return this._http.get<FlagServiceResponse>(flagUrl)
+  }
+}
+
+interface FlagServiceResponse {
+
 }
