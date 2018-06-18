2c1082f6c1062a56848c8ed696a1409db101193c
Corbfon <corbin.fonville@gmail.com>
2018-06-18 12:22:58 -0500

    swap featureFlags over to FlagService functionality

--- src/app/shared/flag.service.ts
+++ src/app/shared/flag.service.ts
@@ -3,31 +3,35 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FlagService {
 
-  /**
-   * A list of all potential flags
-   *  can be read/set using public methods from this service
-   */
-  private _flags: Flags = {
-    pcUpload: false,
-    unaffiliated: false
-  }
+  // /**
+  //  * A list of all potential flags
+  //  *  can be read/set using public methods from this service
+  //  */
+  // private _flags: Flags = {
+  //   pcUpload: false,
+  //   unaffiliated: false
+  // }
+  pcUpload: false
+  unaffiliated: false
 
   constructor() {
   }
 
-  get flags(): Flags {
-    return this._flags
-  }
+  // // this being the name of the property provides a little bit of awkward syntax while calling later,
+  // //  but I couldn't think of anything better
+  // getFlag(flagName: string): boolean {
+  //   return this._flags[flagName]
+  // }
 
-  /**
-   * Used to set flag values from other components/services
-   * @param key the name of the flag being set
-   * @param value defaults to true, the value which should be set to the flag
-   */
-  public setFlag(key: string, value?: boolean): void {
-    if (!value) { value = true }
-    this._flags[key] = value
-  }
+  // /**
+  //  * Used to set flag values from other components/services
+  //  * @param key the name of the flag being set
+  //  * @param value defaults to true, the value which should be set to the flag
+  //  */
+  // public setFlag(flagName: string, value?: boolean): void {
+  //   if (!value) { value = true }
+  //   this._flags[flagName] = value
+  // }
 }
 
 interface Flags {
