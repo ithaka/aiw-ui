b05c3ae3de2e06af3cd69fd5f4f1eaeaae1e9be6
jialinhe96 <jialin.he@ithaka.org>
2018-06-26 17:07:40 -0400

    WIP Browse Groups Page: Set Feature Flag

--- src/app/browse-page/browse-groups/groups.component.ts
+++ src/app/browse-page/browse-groups/groups.component.ts
@@ -22,6 +22,7 @@ export class BrowseGroupsComponent implements OnInit {
   private tags: Tag[] = []
   private groups: any[] = []
   private loading: boolean = true
+  private showCardView: boolean = false
 
   private pagination: {
     totalPages: number,
@@ -106,6 +107,17 @@ export class BrowseGroupsComponent implements OnInit {
         level: 'shared'
       })
     }
+    
+    // Subscribe to asset search params
+    this.subscriptions.push(
+      this.route.params
+      .subscribe((params: Params) => {
+        // Find feature flags
+        if(params && params['featureFlag'] && params['featureFlag'] === 'cardview'){
+              this.showCardView = true;
+        }
+      })
+    )
   } // OnInit
 
   ngOnDestroy() {
