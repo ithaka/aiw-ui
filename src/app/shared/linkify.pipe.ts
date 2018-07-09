c3a252c187a7b2cc7acfa6a09f96d351f16c2288
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-07-09 22:55:53 +0500

    Update link regex!

--- src/app/shared/linkify.pipe.ts
+++ src/app/shared/linkify.pipe.ts
@@ -5,6 +5,6 @@ var linkifyHtml = require('linkifyjs/html');
 @Pipe({name: 'linkify'})
 export class LinkifyPipe implements PipeTransform {
   transform(str: string): string {
-    return str ? linkifyHtml(str, { target: '_blank', validate: { url: function (value) { return /^(http|ftp)s?:\/\//.test(value) } } }) : str;
+    return str ? linkifyHtml(str, { target: '_blank', validate: { url: function (value) { return /^(http[s]?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-\.@:%_+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?/.test(value) } } }) : str;
   }
 }
