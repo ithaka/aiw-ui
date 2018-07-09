aff7f6035ddf588e31a6fad67e668f4fab2147c2
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-07-09 17:38:35 +0500

    Linkify asset metadata only if the content contains the protocol info as well

--- src/app/shared/linkify.pipe.ts
+++ src/app/shared/linkify.pipe.ts
@@ -5,6 +5,6 @@ var linkifyHtml = require('linkifyjs/html');
 @Pipe({name: 'linkify'})
 export class LinkifyPipe implements PipeTransform {
   transform(str: string): string {
-    return str ? linkifyHtml(str, {target: '_blank'}) : str;
+    return str ? linkifyHtml(str, { target: '_blank', validate: { url: function (value) { return /^(http|ftp)s?:\/\//.test(value) } } }) : str;
   }
 }
