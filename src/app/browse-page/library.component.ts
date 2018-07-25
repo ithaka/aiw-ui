3387693df9eb9c99aa1721f8cbf125c58b3dbddd
jialinhe96 <jialin.he@ithaka.org>
2018-07-25 10:11:14 -0400

    Library Component: Check Existence of a Third Facet before Check its Title

--- src/app/browse-page/library.component.ts
+++ src/app/browse-page/library.component.ts
@@ -126,7 +126,7 @@ export class LibraryComponent implements OnInit {
         // Fetch browse collection object from local storage & check if the required collection list has already been set
         let storageBrwseColObj = this._storage.get('browseColObject')
         
-        let hasCategoryTitles = storageBrwseColObj && storageBrwseColObj['categoryid'] && storageBrwseColObj['categoryid'][2].title.length > 0
+        let hasCategoryTitles = storageBrwseColObj && storageBrwseColObj['categoryid'] && storageBrwseColObj['categoryid'][2] && storageBrwseColObj['categoryid'][2].title.length > 0
         if( storageBrwseColObj && storageBrwseColObj[facetType] && hasCategoryTitles){
           if(facetType === 'artstor-geography'){
             this.hierarchicalFacets = storageBrwseColObj[facetType]
