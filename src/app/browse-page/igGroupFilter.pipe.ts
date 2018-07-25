cb357ce03829d02d888d75224a46c0320033fb94
jialinhe96 <jialin.he@ithaka.org>
2018-07-25 10:05:36 -0400

    Image Group Filter Pipe: Check the Existence of Title to Make Sure Image Groups Load for Classification and Geography Page

--- src/app/browse-page/igGroupFilter.pipe.ts
+++ src/app/browse-page/igGroupFilter.pipe.ts
@@ -3,7 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'iggroupfilter',  pure: false })
 export class IgGroupFilterPipe implements PipeTransform {
     transform(items: any[], filter: string): any {
-        if (!items || !filter) {
+        // Currently check the third item's title, because on prod the first two items doesn't have a title while they should have, if we can resolve the problem when should check the first item
+        // And the reason for checking the title is to make sure image groups load for Classification and Geography page
+        if (!items.length || !filter || !items[2].title) {
             return items;
         }
         return items.filter(item => item.title.toLowerCase().indexOf(filter.toLowerCase()) > -1)
