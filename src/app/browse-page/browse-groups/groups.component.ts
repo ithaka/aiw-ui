65a8ecac5fdd196c665c014c9eae5290a781d720
jialinhe96 <jialin.he@ithaka.org>
2018-08-06 12:06:09 -0400

    Browse Image Group: Show Number of Results

--- src/app/browse-page/browse-groups/groups.component.ts
+++ src/app/browse-page/browse-groups/groups.component.ts
@@ -25,6 +25,8 @@ export class BrowseGroupsComponent implements OnInit {
   private loading: boolean = true
   private showCardView: boolean = false
   private showAccessDeniedModal: boolean = false
+  private isSearch: boolean = false
+  private numResultMsg: string = ''
 
   private pagination: {
     totalPages: number,
@@ -149,10 +151,12 @@ export class BrowseGroupsComponent implements OnInit {
 
       // set the term every time there is one in the url
       if (query.term) {
+        this.isSearch = true
         this.searchTerm = query.term
         groupQuery.term = query.term
         this.updateSearchTerm.emit(query.term) // sets the term in the search box
       } else {
+        this.isSearch = false
         this.updateSearchTerm.emit('')
       }
 
@@ -294,6 +298,18 @@ export class BrowseGroupsComponent implements OnInit {
     .take(1)
     .subscribe(
       (data)  => {
+        if (data.total !== 0){
+          if (this.pagination.size < data.total) {
+            this.numResultMsg = 'Showing ' + this.pagination.size + ' of ' + data.total + ' results for \"' + this.searchTerm + '\"'
+          }
+          else {
+            this.numResultMsg = 'Showing ' + data.total + ' of ' + data.total + ' results for \"' + this.searchTerm + '\"'
+          }
+        }
+        else {
+          this.numResultMsg = '0 results to show for \"' + this.searchTerm + '\"'
+        }
+        
         this.pagination.page = groupQuery.page
         this.pagination.totalPages = Math.ceil(data.total/this.pagination.size) // update pagination, which is injected into pagination component
         if (this.pagination.page > this.pagination.totalPages && data.total > 0) {
