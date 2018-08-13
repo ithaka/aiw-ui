a829907f7c37a6187e556ba4dbd18f16ff0a7512
bfraley <brett.fraley@ithaka.org>
2018-08-13 17:24:42 -0400

    Cleanup for style consistency (code factor), group-api pact

--- src/app/pact/group-api.pact.spec.ts
+++ src/app/pact/group-api.pact.spec.ts
@@ -22,34 +22,34 @@ describe('Group Calls #pact', () => {
 
     // Image Group with id of 44d14fe7-13ec-4d84-b911-7ee3ffc4b0cb
     const expectedImageGroupObject = {
-      "description": "",
-      "owner_name": "air01@artstor.org",
-      "tags": ["adl", "collection"],
-      "owner_id": "706217",
-      "sequence_number": 0,
-      "update_date": "2018-01-16T18:49:07Z",
-      "name": "ADL_Group1_copy",
-      "public": false,
-      "creation_date": "2018-01-16T18:48:43Z",
-      "id": "44d14fe7-13ec-4d84-b911-7ee3ffc4b0cb",
-      "access": [
+      'description': '',
+      'owner_name': 'air01@artstor.org',
+      'tags': ['adl', 'collection'],
+      'owner_id': '706217',
+      'sequence_number': 0,
+      'update_date': '2018-01-16T18:49:07Z',
+      'name': 'ADL_Group1_copy',
+      'public': false,
+      'creation_date': '2018-01-16T18:48:43Z',
+      'id': '44d14fe7-13ec-4d84-b911-7ee3ffc4b0cb',
+      'access': [
         {
-          "entity_type": 100,
-          "entity_identifier": "706217",
-          "access_type": 300
+          'entity_type': 100,
+          'entity_identifier': '706217',
+          'access_type': 300
         }
       ],
-      "items": [
-        "AWAYNEIG_10311326670",
-        "ADAVISIG_10311277805",
-        "AWSS35953_35953_38398951",
-        "HARTILL_12324316",
-        "ASITESPHOTOIG_10312738558",
-        "AWSS35953_35953_38398953",
-        "HARTILL_12326634",
-        "HCAP_10310729952",
-        "HCAP_10310728522",
-        "ASITESPHOTOIG_10313835802"
+      'items': [
+        'AWAYNEIG_10311326670',
+        'ADAVISIG_10311277805',
+        'AWSS35953_35953_38398951',
+        'HARTILL_12324316',
+        'ASITESPHOTOIG_10312738558',
+        'AWSS35953_35953_38398953',
+        'HARTILL_12326634',
+        'HCAP_10310729952',
+        'HCAP_10310728522',
+        'ASITESPHOTOIG_10313835802'
       ]
     }
 
@@ -59,7 +59,7 @@ describe('Group Calls #pact', () => {
       matcherPrivateGroupListObject[key] = Matchers.somethingLike(expectedPrivateGroupList[key])
     })
 
-    // Verify the types of the properties - Individual Image Group
+    // Verify types of response properties - Individual Image Group
     let matcherImageGroupObject = {}
     Object.keys(expectedImageGroupObject).forEach((key) => {
       matcherImageGroupObject[key] = Matchers.somethingLike(expectedImageGroupObject[key])
@@ -122,7 +122,7 @@ describe('Group Calls #pact', () => {
         })
       })
 
-      it("should return a list of private group objects", function(done) {
+      it('should return a list of private group objects', function(done) {
         // Run the tests
         _groupService.getAll('private', 48, 0, [], '', '', 'alpha', 'asc')
           .subscribe(res => {
@@ -159,7 +159,7 @@ describe('Group Calls #pact', () => {
     /**
      * Mock and test a group/id enpoint (a single group)
      */
-    describe("GET /api/v1/group/{id}", () => {
+    describe('GET /api/v1/group/{id}', () => {
       beforeAll((done) => {
 
         provider.addInteraction({
@@ -186,7 +186,7 @@ describe('Group Calls #pact', () => {
           })
       })
 
-      it("should return an image group object",
+      it('should return an image group object',
         function (done) {
 
           _groupService.get('44d14fe7-13ec-4d84-b911-7ee3ffc4b0cb')
