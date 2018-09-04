8264995401115a03b77622d84042422f796fe65b
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-09-04 16:01:57 +0500

    Asset Page: Allow users to navigate past the first / last assets in fullscreen quizShuffle mode

--- src/app/asset-page/asset-page.component.ts
+++ src/app/asset-page/asset-page.component.ts
@@ -562,11 +562,11 @@ export class AssetPage implements OnInit, OnDestroy {
     }
 
     private showPrevAsset(): void {
-        if (this.assetNumber > 1) {
+        if (this.quizShuffle || (this.assetNumber > 1)) {
             // Update browse direction
             this.browseAssetDirection = 'prev'
 
-            if ((this.assetIndex > 0)) {
+            if (this.quizShuffle || (this.assetIndex > 0)) {
                 let prevAssetIndex = this.quizShuffle ? Math.floor(Math.random() * this.prevAssetResults.thumbnails.length) + 0 : this.assetIndex - 1; // Assign random thumbnail index if quiz shuffle is true
                 let queryParams = {}
                 if (this.prevRouteTS) {
@@ -586,11 +586,11 @@ export class AssetPage implements OnInit, OnDestroy {
     }
 
     private showNextAsset(): void {
-        if (this.assetNumber < this.totalAssetCount) {
+        if (this.quizShuffle || (this.assetNumber < this.totalAssetCount)) {
             // Update browse direction
             this.browseAssetDirection = 'next'
 
-            if ((this.prevAssetResults.thumbnails) && (this.assetIndex < (this.prevAssetResults.thumbnails.length - 1))) {
+            if ((this.prevAssetResults.thumbnails) && ( this.quizShuffle || ( this.assetIndex < (this.prevAssetResults.thumbnails.length - 1)) )) {
                 let nextAssetIndex = this.quizShuffle ? Math.floor(Math.random() * this.prevAssetResults.thumbnails.length) + 0 : this.assetIndex + 1; // Assign random thumbnail index if quiz shuffle is true
                 let queryParams = {}
                 if (this.prevRouteTS) {
