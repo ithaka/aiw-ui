816e5fdb278fee2b3784142e12935db54e711f80
codiak <592344+codiak@users.noreply.github.com>
2018-09-04 15:26:35 -0500

    Script Store: Comment clean up

--- src/app/shared/script.store.ts
+++ src/app/shared/script.store.ts
@@ -6,5 +6,3 @@ export const ScriptStore: Scripts[] = [
     { name: 'zendesk', src: '../../assets/js/zendesk.js' },
     { name: 'ethnio', src: '//ethn.io/92143.js' }
 ];
-
-// <script type="text/javascript" language="javascript" src="//ethn.io/92143.js" async="true" charset="utf-8"></script>
