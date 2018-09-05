b5157b8436dacfdad0a90dc119c8bb1b3be11068
Corbfon <corbin.fonville@gmail.com>
2018-09-05 09:08:52 -0500

    add typescript form for account page

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -1,9 +1,10 @@
-import { Subscription } from 'rxjs/Rx';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx'
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 
 // Project Dependencies
-import { AuthService } from '../shared';
+import { AuthService } from '../shared'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-account-page',
@@ -16,26 +17,37 @@ export class AccountPage implements OnInit {
   private subscriptions: Subscription[] = [];
 
   private showChangePassModal: boolean = false
+  private accountUpdateForm: FormGroup
 
-  constructor(private _auth: AuthService, private _router: Router) {
+  constructor(
+    private _auth: AuthService,
+    private _router: Router,
+    _fb: FormBuilder
+  ) {
+    this.user = this._auth.getUser()
+
+    this.accountUpdateForm = _fb.group({
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      departmentRole: null,
+      department: null
+    })
   }
 
   ngOnInit() {
-    this.user = this._auth.getUser();
-
     if (!this.user.isLoggedIn) {
-      this._router.navigate(['/home']);
+      this._router.navigate(['/home'])
     }
 
     this.subscriptions.push(
       this._auth.getInstitution().subscribe((institutionObj) => {
-        this.institutionObj = institutionObj;
+        this.institutionObj = institutionObj
       })
     );
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach((sub) => { sub.unsubscribe(); });
+    this.subscriptions.forEach((sub) => { sub.unsubscribe() });
   }
 
 }
