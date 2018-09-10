72bb974f64320d4decc321322e0bdb4bde404175
Corbfon <corbin.fonville@gmail.com>
2018-09-10 12:58:50 -0500

    update setter for departmentRole and department on account-page.component
    old default value setter for form did not work, now manually set the values in ngOnInit

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -1,15 +1,15 @@
-import { Subscription } from "rxjs/Rx";
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Subscription } from "rxjs/Rx"
+import { Component, OnInit } from "@angular/core"
+import { Router } from "@angular/router"
 
 // Project Dependencies
-import { AuthService, AccountService } from "../shared";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { AuthService, AccountService } from "../shared"
+import { FormBuilder, FormGroup } from "@angular/forms"
 import {
   USER_ROLES,
   USER_DEPTS,
   UserRolesAndDepts
-} from "./../register/user-roles";
+} from "./../register/user-roles"
 
 @Component({
   selector: "app-account-page",
@@ -17,23 +17,23 @@ import {
   styleUrls: ["./account-page.component.scss"]
 })
 export class AccountPage implements OnInit {
-  private user: any = {};
-  private institutionObj: any = {};
-  private subscriptions: Subscription[] = [];
+  private user: any = {}
+  private institutionObj: any = {}
+  private subscriptions: Subscription[] = []
 
-  private showChangePassModal: boolean = false;
-  private accountUpdateForm: FormGroup;
+  private showChangePassModal: boolean = false
+  private accountUpdateForm: FormGroup
 
   // ui display controls
-  private updateLoading: boolean = false;
+  private updateLoading: boolean = false
   private messages: {
-    updateSuccess?: boolean;
-    updateError?: boolean;
-  } = {};
+    updateSuccess?: boolean
+    updateError?: boolean
+  } = {}
 
   // update form select field values
-  private userDepts: UserRolesAndDepts[] = [];
-  private userRoles: UserRolesAndDepts[] = [];
+  private userDepts: UserRolesAndDepts[] = []
+  private userRoles: UserRolesAndDepts[] = []
 
   constructor(
     private _account: AccountService,
@@ -41,64 +41,67 @@ export class AccountPage implements OnInit {
     private _router: Router,
     _fb: FormBuilder
   ) {
-    this.user = this._auth.getUser();
+    this.user = this._auth.getUser()
 
-    console.log(this.user.dept);
+    console.log(this.user.dept)
     this.accountUpdateForm = _fb.group({
       firstName: this.user.firstName,
       lastName: this.user.lastName,
       departmentRole: [this.user.role],
       department: [this.user.dept]
-    });
+    })
   }
 
   ngOnInit() {
     if (!this.user.isLoggedIn) {
-      this._router.navigate(["/home"]);
+      this._router.navigate(["/home"])
     }
 
     this.subscriptions.push(
       this._auth.getInstitution().subscribe(institutionObj => {
-        this.institutionObj = institutionObj;
+        this.institutionObj = institutionObj
       })
-    );
+    )
 
     // Issues with unauthorized access to the service, and the fact that the data NEVER changes, led us to hardcode these values:
-    this.userDepts = USER_DEPTS;
-    this.userRoles = USER_ROLES;
+    this.userDepts = USER_DEPTS
+    this.userRoles = USER_ROLES
+
+    this.accountUpdateForm.controls.departmentRole.setValue(this.user.role)
+    this.accountUpdateForm.controls.department.setValue(this.user.dept)
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => {
-      sub.unsubscribe();
-    });
+      sub.unsubscribe()
+    })
   }
 
   submitAccountUpdate(form: FormGroup): void {
-    this.messages = {};
+    this.messages = {}
     if (!form.valid) {
-      return;
+      return
     } // I don't think this would actually get hit, but it's here just in case
 
-    this.updateLoading = true;
+    this.updateLoading = true
 
     // get a copy of the current user value, modify it in memory, send it in the update, and then save it back to local storage
-    let updateUser = this._auth.getUser();
-    Object.assign(updateUser, form.value);
+    let updateUser = this._auth.getUser()
+    Object.assign(updateUser, form.value)
     this._account
       .update(updateUser)
       .take(1)
       .subscribe(
         res => {
-          this.updateLoading = false;
-          this._auth.saveUser(updateUser);
-          this.messages.updateSuccess = true;
+          this.updateLoading = false
+          this._auth.saveUser(updateUser)
+          this.messages.updateSuccess = true
         },
         err => {
-          this.updateLoading = false;
-          console.error(err);
-          this.messages.updateError = true;
+          this.updateLoading = false
+          console.error(err)
+          this.messages.updateError = true
         }
-      );
+      )
   }
 }
