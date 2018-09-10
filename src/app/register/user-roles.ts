d3ca10ebfdef4ebb1b70a0c7af760a211d50743f
codefactor-io <support@codefactor.io>
2018-09-10 21:30:12 +0000

    [CodeFactor] Apply fixes

--- src/app/register/user-roles.ts
+++ src/app/register/user-roles.ts
@@ -1,182 +1,182 @@
 export const USER_ROLES: UserRolesAndDepts[] = [{
-  "id": "ROLE_CU_ADMIN",
-  "desc": "College/University Administrator"
+  'id': 'ROLE_CU_ADMIN',
+  'desc': 'College/University Administrator'
 }, {
-  "id": "ROLE_CU_FACULTY",
-  "desc": "College/University Faculty"
+  'id': 'ROLE_CU_FACULTY',
+  'desc': 'College/University Faculty'
 }, {
-  "id": "ROLE_CU_GRAD_STUDENT",
-  "desc": "College/University Graduate Student"
+  'id': 'ROLE_CU_GRAD_STUDENT',
+  'desc': 'College/University Graduate Student'
 }, {
-  "id": "ROLE_CU_TECH_STAFF",
-  "desc": "College/University Instructional Technology Staff"
+  'id': 'ROLE_CU_TECH_STAFF',
+  'desc': 'College/University Instructional Technology Staff'
 }, {
-  "id": "ROLE_CU_LIBRARIAN",
-  "desc": "College/University Librarian"
+  'id': 'ROLE_CU_LIBRARIAN',
+  'desc': 'College/University Librarian'
 }, {
-  "id": "ROLE_CU_OTHER_STAFF",
-  "desc": "College/University Other Staff"
+  'id': 'ROLE_CU_OTHER_STAFF',
+  'desc': 'College/University Other Staff'
 }, {
-  "id": "ROLE_CU_UNDERGRAD_STUDENT",
-  "desc": "College/University Undergraduate student"
+  'id': 'ROLE_CU_UNDERGRAD_STUDENT',
+  'desc': 'College/University Undergraduate student'
 }, {
-  "id": "ROLE_CU_VISUAL_RES_STAFF",
-  "desc": "College/University Visual Resources Professional"
+  'id': 'ROLE_CU_VISUAL_RES_STAFF',
+  'desc': 'College/University Visual Resources Professional'
 }, {
-  "id": "ROLE_SCHOLAR",
-  "desc": "Independent Scholar"
+  'id': 'ROLE_SCHOLAR',
+  'desc': 'Independent Scholar'
 }, {
-  "id": "ROLE_MUSE_CURATOR",
-  "desc": "Museum Curator"
+  'id': 'ROLE_MUSE_CURATOR',
+  'desc': 'Museum Curator'
 }, {
-  "id": "ROLE_MUSE_EDUCATOR",
-  "desc": "Museum Educator"
+  'id': 'ROLE_MUSE_EDUCATOR',
+  'desc': 'Museum Educator'
 }, {
-  "id": "ROLE_MUSE_VISUAL_RES_STAFF",
-  "desc": "Museum Library/Visual Resources Staff"
+  'id': 'ROLE_MUSE_VISUAL_RES_STAFF',
+  'desc': 'Museum Library/Visual Resources Staff'
 }, {
-  "id": "ROLE_MUSE_OTHER_STAFF",
-  "desc": "Museum Other Staff"
+  'id': 'ROLE_MUSE_OTHER_STAFF',
+  'desc': 'Museum Other Staff'
 }, {
-  "id": "ROLE_PS_LIBRARIAN",
-  "desc": "Primary/Secondary School Librarian"
+  'id': 'ROLE_PS_LIBRARIAN',
+  'desc': 'Primary/Secondary School Librarian'
 }, {
-  "id": "ROLE_PS_OTHER_STAFF",
-  "desc": "Primary/Secondary School Other Staff"
+  'id': 'ROLE_PS_OTHER_STAFF',
+  'desc': 'Primary/Secondary School Other Staff'
 }, {
-  "id": "ROLE_PS_STUDENT",
-  "desc": "Primary/Secondary School Student"
+  'id': 'ROLE_PS_STUDENT',
+  'desc': 'Primary/Secondary School Student'
 }, {
-  "id": "ROLE_PS_TEACHER",
-  "desc": "Primary/Secondary School Teacher"
+  'id': 'ROLE_PS_TEACHER',
+  'desc': 'Primary/Secondary School Teacher'
 }, {
-  "id": "ROLE_PL_LIBRARIAN_OTHERSTAFF",
-  "desc": "Public Library Librarian / Other Staff"
+  'id': 'ROLE_PL_LIBRARIAN_OTHERSTAFF',
+  'desc': 'Public Library Librarian / Other Staff'
 }, {
-  "id": "ROLE_OTHER",
-  "desc": "Other"
+  'id': 'ROLE_OTHER',
+  'desc': 'Other'
 }]
 
 export const USER_DEPTS: UserRolesAndDepts[] = [{
-  "id": "DEPT_AMERI_STUDIES",
-  "desc": "American Studies"
+  'id': 'DEPT_AMERI_STUDIES',
+  'desc': 'American Studies'
 }, {
-  "id": "DEPT_ANTHRO",
-  "desc": "Anthropology"
+  'id': 'DEPT_ANTHRO',
+  'desc': 'Anthropology'
 }, {
-  "id": "DEPT_ARCHAEO",
-  "desc": "Archaeology"
+  'id': 'DEPT_ARCHAEO',
+  'desc': 'Archaeology'
 }, {
-  "id": "DEPT_ARCHITEC",
-  "desc": "Architecture"
+  'id': 'DEPT_ARCHITEC',
+  'desc': 'Architecture'
 }, {
-  "id": "DEPT_AREA_CLUTURE_STUDIES",
-  "desc": "Area, Ethnic, Cultural and Gender Studies"
+  'id': 'DEPT_AREA_CLUTURE_STUDIES',
+  'desc': 'Area, Ethnic, Cultural and Gender Studies'
 }, {
-  "id": "DEPT_ARTHISTORY",
-  "desc": "Art History/Architectural History"
+  'id': 'DEPT_ARTHISTORY',
+  'desc': 'Art History/Architectural History'
 }, {
-  "id": "DEPT_HEALTH_SCIENCES",
-  "desc": "Biology/Medicine/Health Sciences"
+  'id': 'DEPT_HEALTH_SCIENCES',
+  'desc': 'Biology/Medicine/Health Sciences'
 }, {
-  "id": "DEPT_BUSINESS",
-  "desc": "Business, Management, Marketing"
+  'id': 'DEPT_BUSINESS',
+  'desc': 'Business, Management, Marketing'
 }, {
-  "id": "DEPT_CHEM",
-  "desc": "Chemistry"
+  'id': 'DEPT_CHEM',
+  'desc': 'Chemistry'
 }, {
-  "id": "DEPT_CLASSICS",
-  "desc": "Classics"
+  'id': 'DEPT_CLASSICS',
+  'desc': 'Classics'
 }, {
-  "id": "DEPT_COMM_JOURNALISM",
-  "desc": "Communication/Journalism"
+  'id': 'DEPT_COMM_JOURNALISM',
+  'desc': 'Communication/Journalism'
 }, {
-  "id": "DEPT_CIS",
-  "desc": "Computer and Information Sciences"
+  'id': 'DEPT_CIS',
+  'desc': 'Computer and Information Sciences'
 }, {
-  "id": "DEPT_DANCE",
-  "desc": "Dance"
+  'id': 'DEPT_DANCE',
+  'desc': 'Dance'
 }, {
-  "id": "DEPT_APPLIED_ARTS",
-  "desc": "Design and Applied Arts"
+  'id': 'DEPT_APPLIED_ARTS',
+  'desc': 'Design and Applied Arts'
 }, {
-  "id": "DEPT_ECONOMICS",
-  "desc": "Economics"
+  'id': 'DEPT_ECONOMICS',
+  'desc': 'Economics'
 }, {
-  "id": "DEPT_EDU",
-  "desc": "Education"
+  'id': 'DEPT_EDU',
+  'desc': 'Education'
 }, {
-  "id": "DEPT_ENG_LITRATURE",
-  "desc": "English Language and Literature"
+  'id': 'DEPT_ENG_LITRATURE',
+  'desc': 'English Language and Literature'
 }, {
-  "id": "DEPT_FILM",
-  "desc": "Film/Video"
+  'id': 'DEPT_FILM',
+  'desc': 'Film/Video'
 }, {
-  "id": "DEPT_FINE_ART",
-  "desc": "Fine Art/Studio Art"
+  'id': 'DEPT_FINE_ART',
+  'desc': 'Fine Art/Studio Art'
 }, {
-  "id": "DEPT_FOREIGN_LITRATURE",
-  "desc": "Foreign Languages and Literature"
+  'id': 'DEPT_FOREIGN_LITRATURE',
+  'desc': 'Foreign Languages and Literature'
 }, {
-  "id": "DEPT_GENDER_STUDIES",
-  "desc": "Gender Studies "
+  'id': 'DEPT_GENDER_STUDIES',
+  'desc': 'Gender Studies '
 }, {
-  "id": "DEPT_GEO",
-  "desc": "Geography"
+  'id': 'DEPT_GEO',
+  'desc': 'Geography'
 }, {
-  "id": "DEPT_GRAPHIC_DESIGN",
-  "desc": "Graphic and Industrial Design"
+  'id': 'DEPT_GRAPHIC_DESIGN',
+  'desc': 'Graphic and Industrial Design'
 }, {
-  "id": "DEPT_HIST",
-  "desc": "History"
+  'id': 'DEPT_HIST',
+  'desc': 'History'
 }, {
-  "id": "DEPT_LAW",
-  "desc": "Law"
+  'id': 'DEPT_LAW',
+  'desc': 'Law'
 }, {
-  "id": "DEPT_LIB_SCIENCE",
-  "desc": "Library Science"
+  'id': 'DEPT_LIB_SCIENCE',
+  'desc': 'Library Science'
 }, {
-  "id": "DEPT_LINGUISTICS",
-  "desc": "Linguistics"
+  'id': 'DEPT_LINGUISTICS',
+  'desc': 'Linguistics'
 }, {
-  "id": "DEPT_MATH_STATS",
-  "desc": "Mathematics and Statistics"
+  'id': 'DEPT_MATH_STATS',
+  'desc': 'Mathematics and Statistics'
 }, {
-  "id": "DEPT_MUSEUM_STUDIES",
-  "desc": "Museum Studies/Museology"
+  'id': 'DEPT_MUSEUM_STUDIES',
+  'desc': 'Museum Studies/Museology'
 }, {
-  "id": "DEPT_MUSIC",
-  "desc": "Music"
+  'id': 'DEPT_MUSIC',
+  'desc': 'Music'
 }, {
-  "id": "DEPT_NATURAL_RES",
-  "desc": "Natural Resources and Conservation"
+  'id': 'DEPT_NATURAL_RES',
+  'desc': 'Natural Resources and Conservation'
 }, {
-  "id": "DEPT_PHILOSOPHY",
-  "desc": "Philosophy"
+  'id': 'DEPT_PHILOSOPHY',
+  'desc': 'Philosophy'
 }, {
-  "id": "DEPT_PHYSICAL_SCIENCE",
-  "desc": "Physical Sciences or Engineering"
+  'id': 'DEPT_PHYSICAL_SCIENCE',
+  'desc': 'Physical Sciences or Engineering'
 }, {
-  "id": "DEPT_POLITICAL_SCIENCE",
-  "desc": "Political Science"
+  'id': 'DEPT_POLITICAL_SCIENCE',
+  'desc': 'Political Science'
 }, {
-  "id": "DEPT_PSYCHOLOGY",
-  "desc": "Psychology"
+  'id': 'DEPT_PSYCHOLOGY',
+  'desc': 'Psychology'
 }, {
-  "id": "DEPT_RELIGIOUS_STUDIES",
-  "desc": "Religious Studies"
+  'id': 'DEPT_RELIGIOUS_STUDIES',
+  'desc': 'Religious Studies'
 }, {
-  "id": "DEPT_SOCIAL_SCIENCES",
-  "desc": "Social Sciences"
+  'id': 'DEPT_SOCIAL_SCIENCES',
+  'desc': 'Social Sciences'
 }, {
-  "id": "DEPT_SOCIOLOGY",
-  "desc": "Sociology "
+  'id': 'DEPT_SOCIOLOGY',
+  'desc': 'Sociology '
 }, {
-  "id": "DEPT_THEATER",
-  "desc": "Theatre"
+  'id': 'DEPT_THEATER',
+  'desc': 'Theatre'
 }, {
-  "id": "DEPT_OTHER",
-  "desc": "Other"
+  'id': 'DEPT_OTHER',
+  'desc': 'Other'
 }]
 
 export interface UserRolesAndDepts {
