d9b6a56595492cbe96f4f1a763eb11b649e923c1
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2018-10-11 17:40:48 +0500

    Browse By ADL Classifications: Filter out categories containing pipe in name field

--- src/app/browse-page/library.component.ts
+++ src/app/browse-page/library.component.ts
@@ -198,8 +198,11 @@ export class LibraryComponent implements OnInit {
                 else return 0
               })
 
-              this.categoryFacets = categoryFacets
-
+              // Filter out categories containing pipe in name field
+              this.categoryFacets = categoryFacets.filter((category) => {
+                return category.name.indexOf('|') === -1
+              })
+              
               this.loading = false
 
               storageBrwseColObj[facetType] = this.categoryFacets
