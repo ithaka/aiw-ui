9c8c155d814e6b2f86bc4aeef788de9a09ff51ef
codiak <592344+codiak@users.noreply.github.com>
2018-10-17 13:43:41 -0500

    ng cli/6: Fix pug loader script

--- ng-add-pug-loader.js
+++ ng-add-pug-loader.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const commonCliConfig = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/common.js';
 const pugRule = '{ test: /.pug$/, use: [ { loader: "apply-loader" }, { loader: "pug-loader" } ] },';
 
-fs.readFile(commonCliConfig, (err, data) => {
+fs.readFileSync(commonCliConfig, (err, data) => {
   if (err) { throw err; }
 
   const configText = data.toString();
@@ -17,6 +17,6 @@ fs.readFile(commonCliConfig, (err, data) => {
   const position = configText.indexOf('rules: [') + 8;
   const output = [configText.slice(0, position), pugRule, configText.slice(position)].join('');
   const file = fs.openSync(commonCliConfig, 'r+');
-  fs.writeFile(file, output);
-  fs.close(file);
+  fs.writeFileSync(file, output);
+  fs.closeSync(file);
 });
