a35e3204ce2b5dc1fa61e0c7b72bf2df26df9c9b
codiak <592344+codiak@users.noreply.github.com>
2018-10-17 13:36:53 -0500

    [yarn install] ng cli/6: Use default main.ts file

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,65 +1,65 @@
-// // Angular 2
-// // rc2 workaround
-// import { enableDebugTools, disableDebugTools } from '@angular/platform-browser';
-// import { ApplicationRef, enableProdMode, ErrorHandler } from '@angular/core';
+// Angular 2
+// rc2 workaround
+import { enableDebugTools, disableDebugTools } from '@angular/platform-browser';
+import { ApplicationRef, enableProdMode, ErrorHandler } from '@angular/core';
 
 
-// // Error tracking utility for sentry.io
-// import * as Raven from 'raven-js';
-// const { version: appVersion } = require('../../package.json');
+// Error tracking utility for sentry.io
+import * as Raven from 'raven-js';
+const { version: appVersion } = require('../../package.json');
 
-// Raven.config('https://9ef1f98534914bf6826e202370d1f627@sentry.io/209953', {
-//   release: appVersion
-// }).install();
-// export class RavenErrorHandler implements ErrorHandler {
-//   handleError(err: any): void {
-//     Raven.captureException(err);
-//   }
-// }
+Raven.config('https://9ef1f98534914bf6826e202370d1f627@sentry.io/209953', {
+  release: appVersion
+}).install();
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    Raven.captureException(err);
+  }
+}
 
-// // Environment Providers
-// let PROVIDERS: any[] = [
-//   // common env directives
-// ];
+// Environment Providers
+let PROVIDERS: any[] = [
+  // common env directives
+];
 
-// // Angular debug tools in the dev console
-// // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
-// let _decorateModuleRef = function identity<T>(value: T): T { return value; };
+// Angular debug tools in the dev console
+// https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
+let _decorateModuleRef = function identity<T>(value: T): T { return value; };
 
-// if ('production' === ENV) {
-//   // Production
-//   disableDebugTools();
-//   enableProdMode();
+if ('production' === ENV) {
+  // Production
+  disableDebugTools();
+  enableProdMode();
 
-//   PROVIDERS = [
-//     ...PROVIDERS,
-//     { provide: ErrorHandler, useClass: RavenErrorHandler }
-//     // custom providers in production
-//   ];
+  PROVIDERS = [
+    ...PROVIDERS,
+    { provide: ErrorHandler, useClass: RavenErrorHandler }
+    // custom providers in production
+  ];
 
-// } else {
+} else {
 
-//   _decorateModuleRef = (modRef: any) => {
-//     const appRef = modRef.injector.get(ApplicationRef);
-//     const cmpRef = appRef.components[0];
+  _decorateModuleRef = (modRef: any) => {
+    const appRef = modRef.injector.get(ApplicationRef);
+    const cmpRef = appRef.components[0];
 
-//     let _ng = (<any>window).ng;
-//     enableDebugTools(cmpRef);
-//     (<any>window).ng.probe = _ng.probe;
-//     (<any>window).ng.coreTokens = _ng.coreTokens;
-//     return modRef;
-//   };
+    let _ng = (<any>window).ng;
+    enableDebugTools(cmpRef);
+    (<any>window).ng.probe = _ng.probe;
+    (<any>window).ng.coreTokens = _ng.coreTokens;
+    return modRef;
+  };
 
-//   // Development
-//   PROVIDERS = [
-//     ...PROVIDERS,
-//     // custom providers in development
-//   ];
+  // Development
+  PROVIDERS = [
+    ...PROVIDERS,
+    // custom providers in development
+  ];
 
-// }
+}
 
-// export const decorateModuleRef = _decorateModuleRef;
+export const decorateModuleRef = _decorateModuleRef;
 
-// export const ENV_PROVIDERS = [
-//   ...PROVIDERS
-// ];
+export const ENV_PROVIDERS = [
+  ...PROVIDERS
+];
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,39 @@
-/*
- * Angular bootstraping
- */
+// /*
+//  * Angular bootstraping
+//  */
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// // import { decorateModuleRef } from 'app/environment';
+// import { bootloader } from '@angularclass/hmr';
+
+// /*
+//  * App Module
+//  * our top level module that holds all of our components
+//  */
+// import { AppModule } from 'app';
+
+// /*
+//  * Bootstrap our Angular app with a top level NgModule
+//  */
+// export function main(): Promise<any> {
+//   return platformBrowserDynamic()
+//     .bootstrapModule(AppModule)
+//     .then(decorateModuleRef)
+//     .catch(err => console.error(err));
+// }
+
+// // needed for hmr
+// // in prod this is replace for document ready
+// bootloader(main);
+
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { decorateModuleRef } from 'app/environment';
-import { bootloader } from '@angularclass/hmr';
 
-/*
- * App Module
- * our top level module that holds all of our components
- */
-import { AppModule } from 'app';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-/*
- * Bootstrap our Angular app with a top level NgModule
- */
-export function main(): Promise<any> {
-  return platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .then(decorateModuleRef)
-    .catch(err => console.error(err));
+if (environment.production) {
+  enableProdMode();
 }
 
-// needed for hmr
-// in prod this is replace for document ready
-bootloader(main);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
