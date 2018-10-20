d36d756012ff2d5918bde4034df4cd43d208d75a
bfraley <brett.fraley@ithaka.org>
2018-10-19 20:13:11 -0400

    create Typings file for third party libs
    Adds krpano module declaration

---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Declare custom typing declarations here for third party libraries
+ * that don't ship with an @types/library package.
+ */
+
+ // TODO BRETT - Make sure krpano is availble on Window
+ // krpano library
+declare module 'src/assets/js/krpano.js'
