ac5f166caee1fabeb91147ef7a4ea459969b1882
codiak <592344+codiak@users.noreply.github.com>
2018-10-29 12:05:09 -0500

    IG Download: Remove references to "igDownloadInfo"

--- src/app/shared/datatypes/image-group.interface.ts
+++ src/app/shared/datatypes/image-group.interface.ts
@@ -28,7 +28,7 @@ export interface ImageGroup {
   items?: any[],
   description?: string,
   // description?: ImageGroupDescription, // this does not naturally come with image groups, but sometimes we attach it
-  igDownloadInfo?: IgDownloadInfo, // we also attach this to image groups when we have it
+  // igDownloadInfo?: IgDownloadInfo, // we also attach this to image groups when we have it
   sequence_number?: number,
   tags?: string[],
   creation_date?: string,
@@ -41,25 +41,6 @@ export interface ImageGroupDescription {
   igNotes: string;
   count: number;
   isFldrOwner: boolean;
-}
-
-export interface IgDownloadInfo {
-  alreadyDwnldImgCnt: number;
-  canCache?: boolean;
-  curAllowedDwnldCnt: number;
-  dwnldDuration?: number;
-  igId?: string;
-  igImgCount: number;
-  igName?: string;
-  images?: string; // this is a big string of concatenated images and resolutions
-  mediaCnt?: number;
-  message?: string;
-  nonPrivateImgCnt?: number;
-  origDwnldLimit?: number;
-  pptExportAllowed: boolean;
-  pubAudioCnt?: number;
-  qtvrCnt?: number;
-  zooms?: boolean;
 }
 
 /**
