e273c246dfdd783820dab6d2e6b07879037cab9d
bfraley <brett.fraley@ithaka.org>
2018-11-01 10:39:52 -0400

    AssetPP Comp: enable print preview for Multiview asset

--- src/app/asset-pp-page/asset-pp-page.component.ts
+++ src/app/asset-pp-page/asset-pp-page.component.ts
@@ -5,7 +5,7 @@ import { Subscription }   from 'rxjs'
 import { map } from 'rxjs/operators'
 
 // Internal Dependencies
-import { AssetSearchService, MetadataService } from './../shared';
+import { AssetSearchService, MetadataService } from './../shared'
 
 @Component({
   selector: 'ang-asset-pp-page',
@@ -14,16 +14,13 @@ import { AssetSearchService, MetadataService } from './../shared';
 })
 
 export class AssetPPPage implements OnInit {
-  public asset: any = {};
-  public metaArray: Array<any> = [];
-
-  private header = new HttpHeaders().set('Content-Type', 'application/json'); // ... Set content type to JSON
-  private options = { headers: this.header, withCredentials: true }; // Create a request option
-
-  private assetId: string;
-
-  private subscriptions: Subscription[] = [];
-
+  public asset: any = {}
+  public metaArray: Array<any> = []
+  private header = new HttpHeaders().set('Content-Type', 'application/json') // ... Set content type to JSON
+  private options = { headers: this.header, withCredentials: true } // Create a request option\
+  private assetId: string
+  private subscriptions: Subscription[] = []
+  public isMultiView: boolean  = false // flag for print preview of multiview asset
 
   constructor(
     private _metadata: MetadataService,
@@ -39,26 +36,32 @@ export class AssetPPPage implements OnInit {
     this.subscriptions.push(
       this.route.params.pipe(
         map(routeParams => {
-        this.assetId = routeParams['assetId'];
-        this.loadAsset();
+        this.assetId = routeParams['assetId']
+        this.loadAsset()
     })).subscribe()
     )
   }
 
   // Load Image Group Assets
   loadAsset(): void{
-    let self = this;
-    this._metadata.getMetadata(this.assetId).pipe(
+    let self = this
+    this._metadata.getMetadata(this.assetId, undefined, false).pipe(
       map(res => {
+
+        // Is this a multiview asset?
+        if (res.metadata[0].image_compound_urls && res.metadata[0].image_compound_urls.length) {
+          this.isMultiView = true
+        }
+
         let assetData = res && res.metadata && res.metadata[0] ? res.metadata[0]['metadata_json'] : []
         for (let data of assetData){
-          let fieldExists = false;
+          let fieldExists = false
 
           for (let metaData of self.metaArray){
             if (metaData['fieldName'] === data.fieldName){
-              metaData['fieldValue'].push(data.fieldValue);
-              fieldExists = true;
-              break;
+              metaData['fieldValue'].push(data.fieldValue)
+              fieldExists = true
+              break
             }
           }
 
@@ -67,15 +70,14 @@ export class AssetPPPage implements OnInit {
               'fieldName': data.fieldName,
               'fieldValue': []
             }
-            fieldObj['fieldValue'].push(data.fieldValue);
-            self.metaArray.push(fieldObj);
+            fieldObj['fieldValue'].push(data.fieldValue)
+            self.metaArray.push(fieldObj)
           }
 
         }
-
-        self.asset = res.metadata[0];
+        self.asset = res.metadata[0]
     }, (err) => {
-        console.error('Unable to load asset metadata.');
+        console.error('Unable to load asset metadata.')
     })).subscribe()
   }
 
