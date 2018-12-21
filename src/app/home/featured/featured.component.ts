f9ace03d90816b75ae7c39789f15d28784838385
codefactor-io <support@codefactor.io>
2018-12-21 15:22:16 +0000

    [CodeFactor] Apply fixes

--- src/app/home/featured/featured.component.ts
+++ src/app/home/featured/featured.component.ts
@@ -12,19 +12,19 @@ import { FeaturedCollection } from './featured-collection';
 })
 export class FeaturedComponent implements OnInit {
 
-  public siteId: string = ""
-  private conf = ""
+  public siteId: string = ''
+
+  public featured: FeaturedCollection[] = [] // Array of collection objects
+
+  // Array index for which collection is the 'primary image'
+  public primaryFeaturedIndex: number = 0
+  private conf = ''
   private user: any
 
   // Determines which type of featured collections to display
   private featuredType: string; // 'COLLECTIONS', 'PUBLIC_COLLECTIONS', or 'SAHARA'
   private base: string
   private headings: string
-
-  public featured: FeaturedCollection[] = [] // Array of collection objects
-
-  // Array index for which collection is the 'primary image'
-  public primaryFeaturedIndex: number = 0
   private skipAutoSlide: boolean = false
 
   // To pause and resume slide show
@@ -32,6 +32,29 @@ export class FeaturedComponent implements OnInit {
 
   constructor(public _appConfig: AppConfig, public _auth: AuthService) {
     this.conf = this._appConfig.config.featuredCollection // 'HOME.FEATURED' in en.json
+  }
+
+  ngOnInit() {
+    this.siteId = this._appConfig.config.siteID
+    this.user = this._auth.getUser();
+    this.headings = this.conf + '.' + 'HEADINGS'
+
+    // Show Public Featured Collections, or Inst Featured Collections
+    if (this.siteId === 'SAHARA') {
+      this.featuredType = 'SAHARA_COLLECTIONS'
+    }
+    else if (this.user.isLoggedIn || this.user.ipAuthed) { // Show ADL featured collections if the user is Logged-in or IP Authed
+      this.featuredType = 'COLLECTIONS'
+    }
+    else {
+      this.featuredType = 'PUBLIC_COLLECTIONS'
+    }
+
+    this.base = this.conf + '.' + this.featuredType + '.'
+    this.initCollections()
+
+    // Start slideshow
+    this.runSlideshow(this.primaryFeaturedIndex)
   }
 
   /**
@@ -80,7 +103,7 @@ export class FeaturedComponent implements OnInit {
           this.skipAutoSlide = false
         }
       }, 9000)
-  } 
+  }
 
   /**
    * runSlideShow - Pause the homepage slideshow
@@ -93,29 +116,6 @@ export class FeaturedComponent implements OnInit {
     if (index === 0 || index === 2){
       this.runSlideshow(this.primaryFeaturedIndex)
     }
-  }
-
-  ngOnInit() {
-    this.siteId = this._appConfig.config.siteID
-    this.user = this._auth.getUser();
-    this.headings = this.conf + '.' + 'HEADINGS'
-
-    // Show Public Featured Collections, or Inst Featured Collections
-    if (this.siteId === 'SAHARA') {
-      this.featuredType = 'SAHARA_COLLECTIONS'
-    }
-    else if (this.user.isLoggedIn || this.user.ipAuthed) { // Show ADL featured collections if the user is Logged-in or IP Authed
-      this.featuredType = 'COLLECTIONS'
-    }
-    else {
-      this.featuredType = 'PUBLIC_COLLECTIONS'
-    }
-
-    this.base = this.conf + '.' + this.featuredType + "."
-    this.initCollections()
-
-    // Start slideshow
-    this.runSlideshow(this.primaryFeaturedIndex)
   }
 
 }
