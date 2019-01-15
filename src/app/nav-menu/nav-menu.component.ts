1e133d1a8159c4affe17c066f7191fd2c23ee64b
bfraley <brett.fraley@ithaka.org>
2019-01-15 14:46:02 -0500

    NavMenu: comment reloadIG, and cleanup

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -208,14 +208,6 @@ export class NavMenu implements OnInit, OnDestroy {
       take(1),
       map(res => {
         this.ig = putGroup
-
-        // We don't need this probably - needs more testing if commenting it out would work
-        // let removeIds: string[] = []
-        // this._assets.getSelectedAssets().forEach((asset) => {
-        //   removeIds.push(asset.objectId)
-        // })
-        // this._assets.removeFromResults(removeIds, this.ig.items.length) // make the call to asset service which will update the asset grid with modified assets and also pass the total # of items for pagination values
-        
         this._assets.selectModeToggle.emit()
         this.reloadIG() // Reload IG assets after deleting selected assets
       }
@@ -239,7 +231,8 @@ export class NavMenu implements OnInit, OnDestroy {
     }
   }
 
-
+  // Will update the asset grid with modified assets and also
+  // pass the total # of items for pagination values
   private reloadIG(): void{
     this.refreshIG.emit();
   }
