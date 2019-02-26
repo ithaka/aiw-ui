ae95533b776da72cc86d57423addaca616c80f98
jialinhe <jialinhe@umich.edu>
2019-02-26 15:45:36 -0500

    Image group page: header style change

---
+++ src/app/shared/group-title/group-title.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { ImageGroup } from './../'
+
+@Component({
+    selector: 'ang-group-title',
+    templateUrl: 'group-title.component.pug',
+    styleUrls: ['./group-title.component.scss']
+  })
+  export class GroupTitleComponent {
+    @Input()
+    public ig: ImageGroup
+
+    public startModalShow: boolean = false
+
+    private driver: any
+    private isBrowser
+
+    constructor(
+    ){ }
+
+}
