8ba84583c14344d20d2a1e92d2df0ec97b1952c6
codiak <codypumper@gmail.com>
2019-03-06 15:52:02 -0600

    SSR: Add AppDynamics health monitoring

--- server.ts
+++ server.ts
@@ -1,9 +1,20 @@
-// These are important and needed before anything else
+// AppDynamics monitoring
+require("appdynamics").profile({
+  controllerHostName: 'Ithaka.saas.appdynamics.com',
+  controllerPort: 443, 
+  controllerSslEnabled: false,  // Set to true if controllerPort is SSL
+  accountName: 'Ithaka', // Required for SaaS accounts
+  accountAccessKey: '8e51d6a16fb3', // Required for SaaS accounts
+  applicationName: 'Artstor_TEST',
+  tierName: 'artstor-ui-ssr', 
+  nodeName: 'artstor-ui-ssr-01'
+ });
+
+// Prereq Dependencies (required first)
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-
+// Dependencies
 import { enableProdMode } from '@angular/core';
-
 import * as express from 'express';
 import * as fs from 'fs';
 import { join } from 'path';
