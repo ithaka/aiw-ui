8ad94d490e4076c27794926ba05addb650b4f187
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2019-03-29 17:42:25 +0500

    Browse groups card view - thumbnail image states!

--- src/app/browse-page/card-view/card-view.component.ts
+++ src/app/browse-page/card-view/card-view.component.ts
@@ -26,6 +26,8 @@ export class CardViewComponent implements OnInit {
   // Tracks whether to expand or collapse the tags exceeding 3 lines
   public tagsCollapsed: boolean = true
 
+  public dataLoaded: boolean = false
+
   constructor(
     private _auth: AuthService,
     private _search: AssetSearchService,
@@ -63,20 +65,25 @@ export class CardViewComponent implements OnInit {
     if (itemIds.length) {
       this._assets.getAllThumbnails(itemIds)
         .then( allThumbnails => {
-          this.thumbnails = allThumbnails.filter((thumbnail) => {
-            return thumbnail.status === 'available'
-          })
+          this.thumbnails = allThumbnails
+          // this.thumbnails = allThumbnails.filter((thumbnail) => {
+          //   return thumbnail.status === 'available'
+          // })
 
-          this.thumbnails.forEach(thumbnail => {
-            if (thumbnail['media']) {
-              thumbnail.thumbnailImgUrl = thumbnail.media.thumbnailSizeOnePath
-            }
-          })
+          // this.thumbnails.forEach(thumbnail => {
+          //   if (thumbnail['media']) {
+          //     thumbnail.thumbnailImgUrl = thumbnail.media.thumbnailSizeOnePath
+          //   }
+          // })
+          this.dataLoaded = true
         })
         .catch( error => {
+          this.dataLoaded = true
           console.error(error)
         })
-      }
+    } else {
+      this.dataLoaded = true
+    }
   }
 
   /**
@@ -163,5 +170,4 @@ export class CardViewComponent implements OnInit {
     Object.assign(queryParams, {'term': imageGroup.owner_name, 'id': imageGroup.owner_id})
     this._router.navigate(['/browse', 'groups'], { queryParams: queryParams })
   }
-
 }
