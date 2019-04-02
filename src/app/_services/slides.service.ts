68cf674b8400b0fa6a9b7ef80a062d236f3c6e7f
bfraley <brett.fraley@ithaka.org>
2019-04-02 13:05:42 -0400

    WIP - Slides: PageProps

--- src/app/_services/slides.service.ts
+++ src/app/_services/slides.service.ts
@@ -60,7 +60,7 @@ export class SlidesService {
   gapi.client['slides'].presentations.create(payload).then(function (response) {
       let presentation = response.result;
       console.log('Created presentation with ID: %s', presentation.presentationId);
-    }, function (response) {
+    }, (response) => {
       console.log('Error: ' + response.result.error.message);
     })
 
@@ -69,6 +69,19 @@ export class SlidesService {
   public preparePresentation(group) {
 
     let imgUrl = "//stage.artstor.org/api/download?imgid=LESSING_ART_10311440667&url=http%3A%2F%2Fimgserver.artstor.net%2Flessing%2Fart%2Flessing_03070168.fpx%2F4tpyHbTBu0nqakCP1Orz_Q%2F1554147326%2F%3Fcell%3D1024%2C1024%26rgnn%3D0%2C0%2C1%2C1%26cvt%3DJPEG&groupid=8acc0cc2-49c9-42d5-bb5d-d7aeae16a8c5"
+
+    let pageBackground = {
+      solidFill: {
+          color: {
+            rgbColor: {
+              red: 0,
+              green: 0,
+              blue: 0
+            }
+          },
+          alpha: 1.0
+      }
+    }
 
     let imageProps = {
       transparency: 0,
@@ -86,18 +99,15 @@ export class SlidesService {
       objectId: 'test-object-id',
       pageType: 'SLIDE',
       pageElements: [ { image: image }],
-      // "pageBackgroundFill": {
-      //   object(PageBackgroundFill)
-      // },
-      // "colorScheme": {
-      //   object(ColorScheme)
-      // }
+      pageProperties: {
+        pageBackgroundFill: pageBackground
+      }
 
     }
 
     return {
       title: group.name,
-      slides: [page]
+      slides: [page, page]
     }
   }
 
