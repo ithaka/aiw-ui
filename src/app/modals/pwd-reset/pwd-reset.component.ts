a9aec2205b3983453337a9e40dafd8876fbf3e8f
bfraley <brett.fraley@ithaka.org>
2019-04-05 17:21:28 -0400

    cleanup

--- src/app/modals/pwd-reset/pwd-reset.component.ts
+++ src/app/modals/pwd-reset/pwd-reset.component.ts
@@ -26,10 +26,6 @@ export class PwdResetModal implements OnInit, AfterViewInit {
   public submitted = false
   public copyKey = 'MODAL.PASSWORD.RESET'
 
-  // Element handles used in template
-  // public submitButton: HTMLElement
-  // public supportLink: HTMLElement
-
   constructor(
     private _auth: AuthService,
     private _fb: FormBuilder
