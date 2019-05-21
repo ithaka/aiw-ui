1816fb24c821abb39ec936ad41ab6814caf48ec1
Muhammad Rehan Abbasi <rehan.abbasi@acit.com>
2019-05-21 18:35:14 +0500

    Asset download: If width/height is greater than 3000px use IIIF URL to download item

--- src/app/shared/datatypes/asset.interface.ts
+++ src/app/shared/datatypes/asset.interface.ts
@@ -99,11 +99,23 @@ export class Asset {
                     // Include "iiif" param in this case
                     downloadLink = data.baseUrl + "/api/download?imgid=" + this.id + "&url=" + encodeURIComponent(url) + "&iiif=true"
                 } else if (data.image_url) {
-                    // Handle images and video thumbnails
-                    let imageServer = 'http://imgserver.artstor.net/'
-                    // Pass Image url to Download Service for processing metadata
-                    let url = imageServer + data.image_url + "?cell=" + data.download_size + "&rgnn=0,0,1,1&cvt=JPEG"
-                    downloadLink = data.baseUrl + "/api/download?imgid=" + this.id + "&url=" + encodeURIComponent(url)
+                    // If image width or height exceed 3000 pixels, use IIIF URL (max 3000px wide) to download the item
+                    if(data.width >= 3000 || data.height >= 3000) {
+                        let url = this.tileSource.replace('.fcgi%3F', '.fcgi?').replace('info.json', '')
+                        if (url.indexOf('//') == 0) {
+                            url = 'https:' + url
+                        }
+                        url += 'full/3000,/0/native.jpg'
+                        
+                        downloadLink = data.baseUrl + "/api/download?imgid=" + this.id + "&url=" + encodeURIComponent( encodeURI(url) ) + "&iiif=true"
+                    } else {
+                        // Handle images and video thumbnails
+                        let imageServer = 'http://imgserver.artstor.net/'
+                        // Pass Image url to Download Service for processing metadata
+                        let url = imageServer + data.image_url + "?cell=" + data.download_size + "&rgnn=0,0,1,1&cvt=JPEG"
+                        downloadLink = data.baseUrl + "/api/download?imgid=" + this.id + "&url=" + encodeURIComponent(url)
+                            
+                    }
                 } else {
                     // nothing happens here because some assets are not allowed to be downloaded
                 }
