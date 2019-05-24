c6bbdb8bf3b521614ba0baff965b5812ac5b7f5d
bfraley <brett.fraley@ithaka.org>
2019-05-24 15:29:49 -0400

    Search Page comp: Add ethnio survey

--- src/app/shared/script.store.ts
+++ src/app/shared/script.store.ts
@@ -6,8 +6,9 @@ interface Scripts {
 export const ScriptStore: Scripts[] = [
   // Zendesk chat widget
   { name: 'zendesk', src: '../../assets/js/zendesk.js' },
-  // Current survey: Zoomed Details
-  { name: 'ethnio-survey', src: '//ethn.io/74606.js' },
+  // Current survey: OIV, and Search
+  { name: 'ethnio-survey', src: '//ethn.io/74606.js' }, // OIV
+  { name: 'ethnio-search-survey', src: '//ethn.io/65777.js' }, // Search
   // Google API (Auth, Slides)
   { name: 'gapi', src: 'https://apis.google.com/js/api.js' }
 ];
