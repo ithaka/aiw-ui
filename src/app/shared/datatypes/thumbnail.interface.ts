590795b9243844077f84fb5bbd5edf74da15dea0
codiak <codypumper@gmail.com>
2019-06-04 15:26:19 -0500

    Thumbnail refactor: Additional clean up

--- src/app/shared/datatypes/thumbnail.interface.ts
+++ src/app/shared/datatypes/thumbnail.interface.ts
@@ -41,39 +41,8 @@ export class AssetThumbnail {
 
 /**
  * Raw thumbnail/asset object returned by:
- * Search Service (Team Sycamore/Search)
+ * -> See RawSearchAsset in asset-search.service.ts
  */
-// export interface SearchServiceThumbnail {
-  // id	664db63b-d45e-3ad3-aad2-a3828dc819f2
-  // doi	10.2307/artstor.12240886
-  // debug_fields	{}
-  // agent	Pollock
-  // artdiscoverysite	[]
-  // artstorid	SCHLES_130736960
-  // artadditionalfields	null
-  // clusterid	null
-  // categoryid	1034302695
-  // collectiontypes	[…]
-  // collectiontypenameid	[…]
-  // collections	[…]
-  // compound_media	null
-  // contributinginstitutionid	1000
-  // date	n.d. (circa 1850-1880)
-  // donatinginstitutionids	[]
-  // frequentlygroupedwith	[]
-  // iap	false
-  // media	{"format":null,"thumbnailSizeOnePath":"imgstor/size1/schles/d0001/sch_007133010551_img0068.jpg","width":2048,"sizeInBytes":null,"downloadSize":1024,"type":null,"icc_profile_location":null,"thumbnailSizeZeroPath":"imgstor/size0/schles/d0001/sch_007133010551_img0068.jpg","filename":"SCHLES_130736960","lps":"schles/d0001","iiif":null,"storId":null,"adlObjectType":10,"height":3072}
-  // name	Unidentified portraits of Boit family women.
-  // partofcluster	false
-  // tokens	[…]
-  // type	art
-  // updatedon	2017-12-14T23:12:25Z
-  // workid	null
-  // year	1865
-  // yearbegin	1850
-  // yearend	1880
-  // additional_Fields	{}
-// }
 
 /**
  * Raw thumbnail/asset object returned by:
