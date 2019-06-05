77fe0a3cd21b440418cc7c4b8312a78ea293614f
bfraley <brett.fraley@ithaka.org>
2019-06-05 11:24:32 -0400

    Thumbnail Template: remove thumbnail.tombstone checks in template aria label @codiak

--- src/app/asset-grid/thumbnail/thumbnail.component.ts
+++ src/app/asset-grid/thumbnail/thumbnail.component.ts
@@ -1,9 +1,9 @@
-import { Router } from '@angular/router';
-import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router'
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core'
 
-import { AssetThumbnail } from './../../shared'
-import { Angulartics2 } from 'angulartics2';
-import { AssetService, AssetSearchService, ThumbnailService } from 'app/_services';
+import { AssetThumbnail } from 'datatypes'
+import { Angulartics2 } from 'angulartics2'
+import { AssetService, AssetSearchService, ThumbnailService } from 'app/_services'
 
 @Component({
   selector: 'ang-thumbnail',
