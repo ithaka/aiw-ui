20f1476366395269603a3d64b83eaaec3f183c72
codiak <codypumper@gmail.com>
2019-08-13 11:16:36 -0500

    Asset metadata links: Use pipe to display creator/subject links

--- src/app/shared/filter-link.pipe.ts
+++ src/app/shared/filter-link.pipe.ts
@@ -3,12 +3,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'filterLink',  pure: false })
 export class FilterLinkPipe implements PipeTransform {
     transform(value: any, filterKey: string): any {
-        let values = value.split(/(?!;).|\|/g)
-        // [routerLink]="['/search', 'artcreator:(' + value + ')']"
+        let values: string[] = []
+        let restoreChar: string = ''
+        // Semicolons - handle splitting
+        values = value.split(/;\s/g)
+        if (values.length > 1) {
+            restoreChar = ';'
+        } else {
+            // Pipes - handle splitting
+            values = value.split(/\|/g)
+        }
+        // Build search links wrapping field value
         let linkHtml = ''
-        values.forEach(value => {
-            linkHtml += `<a href="/#/search;${filterKey}:(${value})">${value}</a>-`
+        values.forEach((value, index) => {
+            if (index == (values.length - 1)) restoreChar = ''
+            linkHtml += `<a href="/#/search;${filterKey}:(${value})">${value}</a>${restoreChar}<span>&ensp;</span>`
         })
         return linkHtml
     }
 }
+ 
