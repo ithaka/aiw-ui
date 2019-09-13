abbcaae8bbfb0f235ed16f678dd521dc6a4bb1f3
Brett Fraley <brett.fraley@ithaka.org>
2019-09-13 15:53:28 -0400

    PromptComponent: add navigateAndSaveRoute

--- src/app/search-page/prompt/prompt.component.ts
+++ src/app/search-page/prompt/prompt.component.ts
@@ -3,6 +3,8 @@ import { Angulartics2 } from 'angulartics2'
 
 // Project Dependencies
 import { AuthService } from '_services'
+import { Router } from '@angular/router'
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'ang-prompt',
@@ -13,11 +15,17 @@ export class PromptComponent implements OnInit {
 
   constructor(
     private _angulartics: Angulartics2,
-    private _auth: AuthService
+    private _auth: AuthService,
+    private _router: Router,
+    private location: Location
   ) {}
 
   ngOnInit() {}
 
+  navigateAndSaveRoute(route: string): void {
+    this._auth.store('stashedRoute', this.location.path(false));
+    this._router.navigate([route]);
+  }
 
   public trackUnaffiliatedInstLogin(): void{
     this._angulartics.eventTrack.next({ properties: { event: 'unaffiliatedInstLogin', category: 'login' }})
