15f00556639c19936ab389438a949ac54fcfcd15
Will Schoener <will.schoener@ithaka.org>
2019-11-04 15:37:19 -0500

    SRCHX-336: reformat file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, OnDestroy, PLATFORM_ID, Inject, Injector } from '@angular/core'
-import { Router } from '@angular/router'
-import { Subscription } from 'rxjs'
-import { map } from 'rxjs/operators'
-import { DeviceDetectorModule, DeviceDetectorService } from 'ngx-device-detector'
-import { isPlatformBrowser } from '@angular/common'
+import {Component, OnInit, OnDestroy, PLATFORM_ID, Inject, Injector} from '@angular/core'
+import {Router} from '@angular/router'
+import {Subscription} from 'rxjs'
+import {map} from 'rxjs/operators'
+import {DeviceDetectorModule, DeviceDetectorService} from 'ngx-device-detector'
+import {isPlatformBrowser} from '@angular/common'
 
 // Project Dependencies
-import { AssetService, AuthService, DomUtilityService } from '_services'
-import { AppConfig } from '../app.service'
-import { Featured } from './featured'
-import { TagsService } from '../browse-page/tags.service';
+import {AssetService, AuthService, DomUtilityService} from '_services'
+import {AppConfig} from '../app.service'
+import {Featured} from './featured'
+import {TagsService} from '../browse-page/tags.service';
 
 declare var initPath: string
 
@@ -26,7 +26,7 @@ declare var initPath: string
 export class Home implements OnInit, OnDestroy {
 
   // Set our default values
-  localState = { value: '' }
+  localState = {value: ''}
   collections = []
   instCollections = []
   institution: any = {}
@@ -66,10 +66,10 @@ export class Home implements OnInit, OnDestroy {
     // this makes the window always render scrolled to the top
     this._router.events.pipe(
       map(() => {
-        // Dummy scrollTo method created for server rendered domino window object
-        window.scrollTo(0, 0);
-      }
-    )).subscribe()
+          // Dummy scrollTo method created for server rendered domino window object
+          window.scrollTo(0, 0);
+        }
+      )).subscribe()
 
     this.showBlog = this._appConfig.config.showHomeBlog
     this.showPrivateCollections = this._appConfig.config.browseOptions.myCol
@@ -84,7 +84,7 @@ export class Home implements OnInit, OnDestroy {
     if (isPlatformBrowser(this.platformId)) {
       // Provide redirects for initPath detected in index.html from inital load
       if (initPath) {
-        this._router.navigateByUrl(initPath, { replaceUrl: true })
+        this._router.navigateByUrl(initPath, {replaceUrl: true})
           .then(result => {
             // Clear variable to prevent further redirects
             initPath = null
@@ -96,14 +96,14 @@ export class Home implements OnInit, OnDestroy {
       let req = this.injector.get('request');
       let ssrRoutes = ['/public/', '/object/']
 
-      ssrRoutes.forEach (route => {
+      ssrRoutes.forEach(route => {
         if (req && req.url && req.url.indexOf(route) >= 0) {
           let assetId = req.url.replace(route, '')
-          this._router.navigate([route, assetId], { skipLocationChange: true })
+          this._router.navigate([route, assetId], {skipLocationChange: true})
             .then(result => {
               console.log(`Handled ${route} route clean url complete: ${result}`)
             })
-          }
+        }
       })
     }
 
@@ -112,9 +112,9 @@ export class Home implements OnInit, OnDestroy {
     this.subscriptions.push(
       this._auth.getInstitution().pipe(
         map(institutionObj => {
-          this.institution = institutionObj
-        }
-      )).subscribe()
+            this.institution = institutionObj
+          }
+        )).subscribe()
     )
 
     this.loaders['collections'] = true;
@@ -127,64 +127,63 @@ export class Home implements OnInit, OnDestroy {
     let allInstitutionIds = {};
 
     this.subscriptions.push(
-        this._auth.currentUser.pipe(
-            map(userObj => {
-              let userObjInstitutionId = userObj.institutionId,
-                  shouldGetCollectionsFromSearchService = userObjInstitutionId && (this.instCollections.length === 0) && !(userObjInstitutionId in allInstitutionIds);
+      this._auth.currentUser.pipe(
+        map(userObj => {
+            let userObjInstitutionId = userObj.institutionId,
+              shouldGetCollectionsFromSearchService = userObjInstitutionId && (this.instCollections.length === 0) && !(userObjInstitutionId in allInstitutionIds);
 
-              if (shouldGetCollectionsFromSearchService) {
-                allInstitutionIds[userObjInstitutionId] = userObjInstitutionId;
+            if (shouldGetCollectionsFromSearchService) {
+              allInstitutionIds[userObjInstitutionId] = userObjInstitutionId;
 
-            let queryType = {};
-            if (this._auth.isPublicOnly()) {
-              queryType['type'] = "commons"
+              let queryType = {};
+              if (this._auth.isPublicOnly()) {
+                queryType['type'] = "commons"
+              } else {
+                queryType['type'] = "institution"
+              }
+              this.instCollections = this.fetchInitTags(queryType)
             }
-            else {
-              queryType['type'] = "institution"
-            }
-            this.instCollections = this.fetchInitTags(queryType)
+          },
+          err => {
+            console.error('Failed to load user object', err)
           }
-        },
-        err => {
-          console.error('Failed to load user object', err)
-        }
-      )).subscribe()
+        )).subscribe()
     ) // end push
 
     // Set session info for Email Artstor link
     if (isPlatformBrowser(this.platformId)) {
       // Load blog posts for home page
       this._assets.getBlogEntries()
-      .then((blogPosts) => {
-        if (Array.isArray(blogPosts)) {
-          // Format blogPosts to extract excerpt if its not available
-          for(let blogPost of blogPosts) {
-            if(blogPost['content'] && blogPost['content']['rendered']) {
-              let excerpt: string = '<div>'
-              let tempElement = document.createElement('div')
-              tempElement.innerHTML = blogPost['content']['rendered']
-              let pTags = tempElement.querySelectorAll('p')
-              for(let i = 0; i < 10; i++) {
-                if(pTags[i] && pTags[i].innerText.length > 120) {
-                  excerpt += pTags[i].innerText.replace('Content:', '')
-                  break
+        .then((blogPosts) => {
+          if (Array.isArray(blogPosts)) {
+            // Format blogPosts to extract excerpt if its not available
+            for (let blogPost of blogPosts) {
+              if (blogPost['content'] && blogPost['content']['rendered']) {
+                let excerpt: string = '<div>'
+                let tempElement = document.createElement('div')
+                tempElement.innerHTML = blogPost['content']['rendered']
+                let pTags = tempElement.querySelectorAll('p')
+                for (let i = 0; i < 10; i++) {
+                  if (pTags[i] && pTags[i].innerText.length > 120) {
+                    excerpt += pTags[i].innerText.replace('Content:', '')
+                    break
+                  }
+                }
+                excerpt += '</div>'
+                if (blogPost['excerpt'] && blogPost['excerpt']['rendered'] === '') {
+                  blogPost['excerpt']['rendered'] = excerpt
                 }
               }
-              excerpt += '</div>'
-              if(blogPost['excerpt'] && blogPost['excerpt']['rendered'] === '') {
-                blogPost['excerpt']['rendered'] = excerpt
-              }
             }
-          }
 
-          this.blogPosts = blogPosts
-        }
-        this.blogLoading = false
-      } )
-      .catch((error) => {
-        console.log(error)
-        this.blogLoading = false
-      });
+            this.blogPosts = blogPosts
+          }
+          this.blogLoading = false
+        })
+        .catch((error) => {
+          console.log(error)
+          this.blogLoading = false
+        });
       // Device info for contact form
       this.fetchDeviceInfo()
     }
@@ -192,7 +191,9 @@ export class Home implements OnInit, OnDestroy {
   } // OnInit
 
   ngOnDestroy() {
-    this.subscriptions.forEach((sub) => { sub.unsubscribe(); });
+    this.subscriptions.forEach((sub) => {
+      sub.unsubscribe();
+    });
   }
 
   private fetchInitTags(queryType: {}): any {
@@ -205,7 +206,7 @@ export class Home implements OnInit, OnDestroy {
       });
   }
 
-  
+
   /**
    * Gets client information for support email
    * @requires browser
