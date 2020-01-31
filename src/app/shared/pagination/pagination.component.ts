a8296d20c9a557ae541c3b16d8c9964ac81fb60f
Mike Iden <mike.iden@ithaka.org>
2020-01-31 09:40:13 -0500

    PLATO-496: Using a large int for a random number to keep some ie11 support

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -14,7 +14,7 @@ export class PaginationComponent implements OnInit {
   @Output() goToPage: EventEmitter<any> = new EventEmitter();
   @Output() paginationFocused: EventEmitter<boolean> = new EventEmitter() // Invoke pagination focused in parent component to close sort dropdown
 
-  private paginationInputId: string = `currentpageinput${Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER))}`;
+  private paginationInputId: string = `currentpageinput${Math.floor(Math.random() * 99999)}`;
 
   constructor() { }
 
