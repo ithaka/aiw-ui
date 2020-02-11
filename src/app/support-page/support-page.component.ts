bcadfcb0bfd9f95372dc60ecb69b26882f3ebd67
Jialin He <jialin.he@ithaka.org>
2020-02-11 10:59:58 -0500

    PLATO-466: Remove unused oninit

--- src/app/support-page/support-page.component.ts
+++ src/app/support-page/support-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-support-page',
@@ -8,8 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class SupportPageComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
