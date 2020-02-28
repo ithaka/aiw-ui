d1cd97bdee2877ccffae78c2e6259ed62a2eb328
Robert Hirsch <robert.hirsch@productops.com>
2020-02-28 11:45:28 -0800

    Removed survey from app component and script store and added error handling

--- src/app/_services/script.store.ts
+++ src/app/_services/script.store.ts
@@ -6,8 +6,6 @@ interface Scripts {
 export const ScriptStore: Scripts[] = [
   // Zendesk chat widget
   { name: 'zendesk', src: '../../assets/js/zendesk.js' },
-  // Ethnio, current survey: Annual demographic survey on image groups, search results, asset page
-  { name: 'ethnio-survey', src: '//ethn.io/49123.js' },
   // Google API (Auth, Slides)
   { name: 'gapi', src: 'https://apis.google.com/js/api.js' }
 ];
