bfd9a3297b30619f5d6cf018ce93ede5068a65f1
Evan Slawski <evan.slawski@ithaka.org>
2021-01-14 15:41:33 -0500

    PLATO-1281: Remove ethino survey

--- src/app/image-group-page/image-group-page.component.ts
+++ src/app/image-group-page/image-group-page.component.ts
@@ -108,9 +108,6 @@ export class ImageGroupPage implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    // Ethnio survey 12/9
-    this._script.loadScript('ethnio-survey')
-
     if (this.unaffiliatedUser) {
       this.showAccessDeniedModal = true
       return
