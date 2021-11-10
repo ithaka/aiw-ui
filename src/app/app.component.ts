def069bf31e7a339b840f82e4229d96716172d4f
Yanni Mouzakis <yanni.mouzakis@ithaka.org>
2021-11-10 16:11:37 -0500

    remove survey from src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -265,8 +265,6 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    // Ethnio survey 11-10-21
-    this._script.loadScript('ethnio-survey');
     if (isPlatformBrowser(this.platformId)) {
       // Setup statusPageClient & subscribe to any status updates to show banner
       import('statuspage-client') // Load StatusPage client -side
