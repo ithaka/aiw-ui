c32727e4d9b876a061e2dd101cb246d83e4ae5b8
Nazim Ali <nazim.ali@ithaka.org>
2023-03-24 17:24:35 -0400

    Add deploy

--- deploy-ssr.sh
+++ deploy-ssr.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-if [ "$1" = "prod" ]; then
-    ENVIRONMENT="prod"
-else
-    ENVIRONMENT="test"
-fi
-APP_NAME="artstor-ui"
-SAGOKU_BUILD_USER="armaan.dandavati@ithaka.org"
 
-uuid=$(uuidgen | tr -d \'\n\' | tr \'[:upper:]\' \'[:lower:]\')
+ENVIRONMENT=$1
+IMAGE_TAG=$2
+COMMiT_SHA=$3
+SAGOKU_BUILD_USER=$4
+
+APP_NAME="artstor-ui"
+
 sagokuURL="http://sagoku.${ENVIRONMENT}.cirrostratus.org/api/docker/${APP_NAME}/deploy"
 sagokuTeam="user=${SAGOKU_BUILD_USER}"
 
 
-curl -sSk --cookie-jar /tmp/cookie -L -F ${sagokuTeam} -F deployId=${uuid} -F image=$2 ${sagokuURL}
+curl -sSk --cookie-jar /tmp/cookie -L -F ${sagokuTeam} -F deployId=${COMMiT_SHA} -F image=${IMAGE_TAG} ${sagokuURL}
