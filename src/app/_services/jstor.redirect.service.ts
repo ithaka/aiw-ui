9ea8f2a594859a5ccde25218833d0a9dc82aac8d
Dane Hillard <github@danehillard.com>
2024-06-21 15:27:55 -0400

    Add flag for redirection

--- src/app/_services/jstor.redirect.service.ts
+++ src/app/_services/jstor.redirect.service.ts
@@ -1,19 +1,51 @@
+import { gql } from '@apollo/client/core';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateChild } from "@angular/router";
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+
+const FLAGS_QUERY = gql`
+  query AiwFlagList($flagsFlagList: [String]) {
+    flags(flagList: $flagsFlagList) {
+      enabled
+    }
+  }
+`;
+
+const APOLLO = new ApolloClient({
+  uri: "/ui/data-fetch/gateway",
+  headers: { authorization: "aiw-ui" },
+});
+
+const REDIRECT_FLAG = "artstor_client_redirect";
 
 @Injectable()
 export class JSTORRedirect implements CanActivateChild {
    canActivateChild(route: ActivatedRouteSnapshot) {
+
+    const options = {
+      query: FLAGS_QUERY,
+      variables: {
+        flagsFlagList: [REDIRECT_FLAG],
+      },
+    } as any;
+    const flags = await APOLLO.query(options);
+    const data = flags.response ? flags.response.data : null;
+    const enabledFlags = data ? data.flags.enabled : [];
+    const doRedirect = enabledFlags.includes(REDIRECT_FLAG);
 
     const currentRequest = window.location.href;
 
     if (currentRequest.includes('/#/')) {
       const params = new URLSearchParams({artstorPath: currentRequest }).toString();
       fetch(`/get-the-redirect-please/?${params}`).then(async resp => {
-        // TODO: Logic to handle "location" response from server and 301 user to that
         const data = await resp.json();
         if (data.location) {
-          console.log('redirecting to:', data.location);
+          if (doRedirect) {
+            console.log('Will redirect to:', data.location);
+            window.location.replace(data.location);
+          } else {
+            console.log('Will redirect to:', data.location);
+          }
         }
 
       });
