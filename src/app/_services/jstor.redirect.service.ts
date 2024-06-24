75d64865093d6858c8ba825dcab02c24c1724b39
Dane Hillard <github@danehillard.com>
2024-06-24 14:09:13 -0400

    Try async approach

--- src/app/_services/jstor.redirect.service.ts
+++ src/app/_services/jstor.redirect.service.ts
@@ -8,57 +8,60 @@ const FLAGS_QUERY = `
     }
   }
 `;
-
 const REDIRECT_FLAG = "artstor_client_redirection";
+const OPTIONS = {
+  operationName: 'AiwFlagList',
+  query: FLAGS_QUERY,
+  variables: {
+    flagsFlagList: [REDIRECT_FLAG],
+  },
+} as any;
 
 @Injectable()
 export class JSTORRedirect implements CanActivateChild {
-   canActivateChild(route: ActivatedRouteSnapshot) {
+  canActivateChild(route: ActivatedRouteSnapshot): Promise<boolean> {
+    return this.checkRedirect();
+  }
 
-    const options = {
-      operationName: 'AiwFlagList',
-      query: FLAGS_QUERY,
-      variables: {
-        flagsFlagList: [REDIRECT_FLAG],
-      },
-    } as any;
-
-    return fetch('/ui/data-fetch/gateway', {
+  async checkRedirect(): Promise<boolean> {
+    const flagResponse = await fetch('/ui/data-fetch/gateway', {
       method: 'POST',
       headers: {
         'authorization': 'aiw-ui',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(options)
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const flagData = data.data ? data.data : null;
-        const enabledFlags = flagData ? flagData.flags.enabled : [];
-        const doRedirect = enabledFlags.includes(REDIRECT_FLAG);
+      body: JSON.stringify(OPTIONS)
+    });
+    const flagData = await flagResponse.json();
+    const flagStates = flagData.data ? flagData.data : null;
+    const enabledFlags = flagStates ? flagStates.flags.enabled : [];
+    const doRedirect = enabledFlags.includes(REDIRECT_FLAG);
 
-        const currentRequest = window.location.href;
+    const currentRequest = window.location.href;
 
-        if (currentRequest.includes('/#/')) {
-          const params = new URLSearchParams({artstorPath: currentRequest }).toString();
+    if (currentRequest.includes('/#/')) {
+      const params = new URLSearchParams({artstorPath: currentRequest }).toString();
 
-          return fetch(`/get-the-redirect-please/?${params}`)
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.location) {
-                if (doRedirect) {
-                  console.log('Redirecting to:', data.location);
-                  window.location.replace(data.location);
-                  return false;
-                } else {
-                  console.log('Will eventually redirect to:', data.location);
-                  return true;
-                }
-              }
-            });
+      const redirectResponse = await fetch(`/get-the-redirect-please/?${params}`);
+      const redirectData = await redirectResponse.json();
+      if (redirectData.location) {
+        if (doRedirect) {
+          console.log('Redirecting to:', redirectData.location);
+          window.location.replace(redirectData.location);
+          return false;  // Redirecting, don't show the page
         } else {
-          return true;
+          console.log('Will eventually redirect to:', redirectData.location);
+          return true;  // Eventually redirecting, but for now show the page
         }
-      });
+      } else {
+        return new Promise((resolve, reject) => {
+          resolve(true);  // No redirect set yet, will need to create
+        });
+      }
+    } else {
+      return new Promise((resolve, reject) => {
+        resolve(true);  // Not a client-redirectable URL
+      }
+    }
   }
 }
